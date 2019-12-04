1f78b996fd62b711daee5c7d4a9478a35aa8c48b
Adam Erickson <adam.michael.erickson@gmail.com>
2019-12-04 16:33:02 -0500

    update

--- isofit/isofit.py
+++ isofit/isofit.py
@@ -80,7 +80,7 @@ class Isofit:
                 line_start, line_end, samp_start, samp_end = ranges
                 self.rows = range(int(row_start), int(row_end))
                 self.cols = range(int(col_start), int(col_end))
-        run()
+        self.run()
 
     def run(self, profile=False):
         """
