8de85457dca08351a0d0a091903317815ba88185
Jay E Fahlen <jay.e.fahlen@jpl.nasa.gov>
2020-03-13 09:53:34 -0700

    Moves download_HRRR from common.py to add_HRRR

--- isofit/utils/add_HRRR_profiles_to_modtran_config.py
+++ isofit/utils/add_HRRR_profiles_to_modtran_config.py
@@ -23,8 +23,12 @@ import scipy as s
 import json
 from copy import deepcopy
 from datetime import date, timedelta
+import urllib.request
+import time
+import os
 
-from isofit.core.common import download_HRRR, get_HRRR_data
+import pygrib
+
 from isofit.core.common import json_load_ascii
 
 class HRRR_to_MODTRAN_profiles():
@@ -142,6 +146,118 @@ class HRRR_to_MODTRAN_profiles():
         self.prof_H2O_dict['UNITS'] = 'UNT_DPPMV'
         self.prof_H2O_dict['PROFILE'] = list(mod_mixing_ratio_ppmV)
 
+
+def reporthook(a, b, c):
+    '''
+    Report download progress in megabytes
+    '''
+    # ',' at the end of the line is important!
+    print("\r % 3.1f%% of %.2f MB\r" % (min(100, float(a * b) / c * 100), c/1000000.), end='')
+
+def download_HRRR(DATE,
+                  model='hrrr',
+                  field='sfc',
+                  hour=range(0, 24),
+                  fxx=range(0, 1),
+                  OUTDIR='./'):
+    '''
+    # Brian Blaylock
+    # February 13, 2018
+
+    # Updated December 10, 2018 for Python 3
+
+    # Modified from original by Jay Fahlen to not download the file if it already exists.
+    # March 4, 2020
+
+    Download archived HRRR files from MesoWest Pando S3 archive system.
+
+    Please register before downloading from our HRRR archive:
+    http://hrrr.chpc.utah.edu/hrrr_download_register.html
+
+    For info on the University of Utah HRRR archive and to see what dates are 
+    available, look here:
+    http://hrrr.chpc.utah.edu/
+
+    Contact:
+    brian.blaylock@utah.edu
+
+    Downloads from the University of Utah MesoWest HRRR archive
+    Input:
+        DATE   - A date object for the model run you are downloading from.
+        model  - The model type you want to download. Default is 'hrrr'
+                 Model Options are ['hrrr', 'hrrrX','hrrrak']
+        field  - Variable fields you wish to download. Default is sfc, surface.
+                 Options are fields ['prs', 'sfc','subh', 'nat']
+        hour   - Range of model run hours. Default grabs all hours of day.
+        fxx    - Range of forecast hours. Default grabs analysis hour (f00).
+        OUTDIR - Directory to save the files.
+
+    Outcome:
+        Downloads the desired HRRR file and renames with date info preceeding
+        the original file name (i.e. 20170101_hrrr.t00z.wrfsfcf00.grib2)
+    '''
+    # Make OUTDIR if path doesn't exist
+    if not os.path.exists(OUTDIR):
+        os.makedirs(OUTDIR)
+
+    # Loop through each hour and each forecast and download.
+    for h in hour:
+        for f in fxx:
+            # 1) Build the URL string we want to download.
+            #    fname is the file name in the format
+            #    [model].t[hh]z.wrf[field]f[xx].grib2
+            #    i.e. hrrr.t00z.wrfsfcf00.grib2
+            fname = "%s.t%02dz.wrf%sf%02d.grib2" % (model, h, field, f)
+            URL = "https://pando-rgw01.chpc.utah.edu/%s/%s/%s/%s" \
+                   % (model, field, DATE.strftime('%Y%m%d'), fname)
+
+            # 2) Rename file with date preceeding original filename
+            #    i.e. 20170105_hrrr.t00z.wrfsfcf00.grib2
+            rename = "%s_%s" \
+                     % (DATE.strftime('%Y%m%d'), fname)
+            
+            filename = OUTDIR+rename
+            if not os.path.exists(filename):
+
+                # 3) Download the file via https
+                # Check the file size, make it's big enough to exist.         
+                check_this = urllib.request.urlopen(URL)
+                file_size = int(check_this.info()['content-length'])
+                if file_size > 10000:
+                    print("Downloading:", URL)
+                    urllib.request.urlretrieve(URL, OUTDIR+rename, reporthook)
+                    print("\n")
+                else:
+                    # URL returns an "Key does not exist" message
+                    print("ERROR:", URL, "Does Not Exist")
+
+                # 4) Sleep five seconds, as a courtesy for using the archive.
+                time.sleep(5)
+    return filename
+
+def get_HRRR_data(filename):
+    grbs = pygrib.open(filename)
+
+    msgs = [str(grb) for grb in grbs]
+
+    string = 'Geopotential Height:gpm'
+    temp = [msg for msg in msgs if msg.find(string) > -1 and msg.find('isobaricInhPa') > -1]
+    pressure_levels_Pa = s.array([int(s.split(' ')[3]) for s in temp])
+
+    geo_pot_height_grbs = grbs.select(name = 'Geopotential Height', \
+                                      typeOfLevel='isobaricInhPa', level=lambda l: l > 0)
+    temperature_grbs = grbs.select(name = 'Temperature', \
+                                   typeOfLevel='isobaricInhPa', level=lambda l: l > 0)
+    rh_grbs = grbs.select(name = 'Relative humidity', \
+                          typeOfLevel='isobaricInhPa', level=lambda l: l > 0)
+
+    lat, lon = geo_pot_height_grbs[0].latlons()
+
+    geo_pot_height = s.stack([grb.values for grb in geo_pot_height_grbs])
+    temperature = s.stack([grb.values for grb in temperature_grbs])
+    rh = s.stack([grb.values for grb in rh_grbs])
+
+    return lat, lon, geo_pot_height, temperature, rh, pressure_levels_Pa
 
 def main():
 
