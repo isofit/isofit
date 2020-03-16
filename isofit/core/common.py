angle-lut fix shelled out, needs modtran/sixs/et degree/radian names

isofit/core/common.py
@@ -41,7 +41,45 @@ eps = 1e-5
 class VectorInterpolator:
     """."""
 
-    def __init__(self, grid, data):
+    def __init__(self, grid, data, lut_interp_types):
+        self.lut_interp_types = lut_interp_types
+
+        # expand grid dimensionality as needed
+        [radian_locations] = np.where(self.lut_interp_types == 'd')
+        [degree_locations] = np.where(self.lut_interp_types == 'r')
+        angle_locations = np.hstack([radian_locations,degree_locations])
+        angle_types = np.hstack([self.lut_interp_types[radian_locations],self.lut_interp_types[degree_locations]])
+        for _angle_loc in range(len(angle_locations)):
+
+            angle_loc = angle_locations[_angle_loc]
+            # get original grid at given location
+            original_grid_subset = np.array(grid[angle_loc])
+
+            # convert for angular coordinates
+            if (angle_types[_angle_loc] == 'r'):
+                grid_subset_cosin = np.cos(original_grid_subset)
+                grid_subset_sin = np.sin(original_grid_subset)
+            elif (angle_types[_angle_loc] == 'd'):
+                grid_subset_cosin = np.cos(original_grid_subset / 180. * np.pi)
+                grid_subset_sin = np.sin(original_grid_subset / 180. * np.pi)
+
+            # handle the fact that the grid may no longer be in order
+            grid_subset_cosin_order = np.argsort(grid_subset_cosin)
+            grid_subset_sin_order = np.argsort(grid_subset_sin)
+
+            # convert current grid location, and add a second
+            grid[angle_loc] = grid_subset_cosin[grid_subset_cosin_order]
+            grid.insert(angle_loc+1, grid_subset_sin[grid_subset_sin_order])
+
+            # now augment data in the same manner
+            original_data_subset = np.array(data[angle_loc])
+            data[angle_loc] = original_data_subset[grid_subset_cosin_order]
+            data.insert(angle_loc+1, original_data_subset[grid_subset_sin_order])
+
+            # update the rest of the angle locations
+            angle_locations += 1
+
+
         self.n = data.shape[-1]
         grid_aug = grid + [np.arange(data.shape[-1])]
         self.itp = RegularGridInterpolator(grid_aug, data,
@@ -49,9 +87,20 @@ class VectorInterpolator:
 
     def __call__(self, points):
 
-        x = np.zeros((self.n,len(points)+1))
+        x = np.zeros((self.n,len(points) + 1 + np.sum(self.lut_interp_types != 'n')))
+        offset_count = 0
         for i in range(len(points)):
-            x[:, i] = points[i]
+            if self.lut_interp_types[i] == 'a':
+                x[:, i + offset_count] = points[i]
+            elif self.lut_interp_types[i] == 'r':
+                x[:, i + offset_count] = np.cos(points[i])
+                x[:, i + 1 + offset_count] = np.sin(points[i])
+                offset_count += 1
+            elif self.lut_interp_types[i] == 'd':
+                x[:, i + offset_count] = np.cos(points[i] / 180. * np.pi)
+                x[:, i + 1 + offset_count] = np.sin(points[i] / 180. * np.pi)
+                offset_count += 1
+
         # This last dimension is always an integer so no
         # interpolation is performed. This is done only
         # for performance reasons.

isofit/radiative_transfer/look_up_tables.py
@@ -72,6 +72,12 @@ class TabularRT:
 
         self.luts = {}
 
+        self.angular_lut_keys_degrees = []
+        self.angular_lut_keys_radians = []
+
+        # set up lookup table grid, and associated filename prefixes
+        self.lut_dims, self.lut_grids, self.lut_names, self.lut_interp_types = [], [], [], []
+
         # Retrieved variables.  We establish scaling, bounds, and
         # initial guesses for each state vector element.  The state
         # vector elements are all free parameters in the RT lookup table,
@@ -91,6 +97,7 @@ class TabularRT:
         self.prior_mean = np.array(self.prior_mean)
         self.prior_sigma = np.array(self.prior_sigma)
 
+
     def build_lut(self, rebuild=False):
         """Each LUT is associated with a source directory.  We build a lookup table by: 
               (1) defining the LUT dimensions, state vector names, and the grid 
@@ -100,17 +107,34 @@ class TabularRT:
               (3) loading the LUTs, one per key atmospheric coefficient vector,
                   into memory as VectorInterpolator objects."""
 
-        # set up lookup table grid, and associated filename prefixes
-        self.lut_dims, self.lut_grids, self.lut_names = [], [], []
-        for key, val in self.lut_grid_config.items():
-            self.lut_names.append(key)
-            self.lut_grids.append(np.array(val))
-            if len(self.lut_grids[-1]) == 1:
-                raise ValueError('Only 1 value in LUT grid {}.  1-d LUT grids cannot be interpreted.'.format(key))
-            self.lut_dims.append(len(val))
-            if val != sorted(val):
+        for key, original_grid_values in self.lut_grid_config.items():
+            # cast as numpy array to be sure
+            original_grid_values = np.array(original_grid_values)
+
+            # do some quick checks on the values
+            if len(original_grid_values) == 1:
+                err = 'Only 1 value in LUT grid {}.  1-d LUT grids cannot be interpreted.'.format(key)
+                raise ValueError(err)
+            if original_grid_values != sorted(original_grid_values):
                 logging.error('Lookup table grid needs ascending order')
                 raise ValueError('Lookup table grid needs ascending order')
+
+            # Store the values
+            self.lut_grids.append(original_grid_values)
+            self.lut_dims.append(len(original_grid_values))
+            self.lut_names.append(key)
+
+            # Store in an indication of the type of value each key is
+            # (normal - n, degree - d, radian - r)
+            if key in self.angular_lut_keys_radians:
+                self.lut_interp_types.append('r')
+            elif key in self.angular_lut_keys_degrees:
+                self.lut_interp_types.append('d')
+            else:
+                self.lut_interp_types.append('n')
+
+        # Cast as array for faster reference later
+        self.lut_interp_types = np.array(self.lut_interp_types)
 
         # "points" contains all combinations of grid points
         # We will have one filename prefix per point