84aeef680edf1e2eb0e9b484785a53c74c61d44a
Adam Erickson <adam.michael.erickson@gmail.com>
2020-04-23 09:50:10 -0400

    mark and comment unused variables

--- isofit/core/inverse.py
+++ isofit/core/inverse.py
@@ -42,8 +42,7 @@ error_code = -1
 class Inversion:
 
     def __init__(self, config, forward):
-        """Initialization specifies retrieval subwindows for calculating
-        measurement cost distributions."""
+        """Specify retrieval subwindows for calculating measurement cost distributions."""
 
         self.lasttime = time.time()
         self.fm = forward
@@ -53,6 +52,7 @@ class Inversion:
         self.windows = config['windows']  # Retrieval windows
         self.state_indep_S_hat = False
         self.simulation_mode = None
+
         if 'simulation_mode' in config:
             self.simulation_mode = config['simulation_mode']
         if 'Cressie_MAP_confidence' in config:
@@ -66,6 +66,7 @@ class Inversion:
             idx = np.where(np.logical_and(self.fm.instrument.wl_init > lo,
                                           self.fm.instrument.wl_init < hi))[0]
             self.winidx = np.concatenate((self.winidx, idx), axis=0)
+
         self.counts = 0
         self.inversions = 0
 
@@ -86,18 +87,23 @@ class Inversion:
                 self.least_squares_params[k] = v
 
     def calc_prior(self, x, geom):
-        """Calculate prior distribution of radiance. This depends on the 
-        location in the state space. Return the inverse covariance and 
-        its square root (for non-quadratic error residual calculation)."""
+        """Calculate prior distribution of radiance.
+
+        Depends on the location in the state space. Return the inverse covariance
+        and its square root (for non-quadratic error residual calculation).
+        """
 
         xa = self.fm.xa(x, geom)
         Sa = self.fm.Sa(x, geom)
         Sa_inv, Sa_inv_sqrt = svd_inv_sqrt(Sa, hashtable=self.hashtable)
+
         return xa, Sa, Sa_inv, Sa_inv_sqrt
 
     def calc_posterior(self, x, geom, meas):
-        """Calculate posterior distribution of state vector. This depends 
-        both on the location in the state space and the radiance (via noise)."""
+        """Calculate posterior distribution of state vector.
+
+        Depends on location in state space and radiance (via noise).
+        """
 
         xa = self.fm.xa(x, geom)
         Sa = self.fm.Sa(x, geom)
@@ -108,8 +114,8 @@ class Inversion:
 
         # Gain matrix G reflects current state, so we use the state-dependent
         # Jacobian matrix K
-        S_hat = svd_inv(K.T.dot(Seps_inv).dot(
-            K) + Sa_inv, hashtable=self.hashtable)
+        S_hat = svd_inv(K.T.dot(Seps_inv).dot(K) + Sa_inv,
+                        hashtable=self.hashtable)
         G = S_hat.dot(K.T).dot(Seps_inv)
 
         # N. Cressie [ASA 2018] suggests an alternate definition of S_hat for
@@ -118,39 +124,46 @@ class Inversion:
             Ka = self.fm.K(xa, geom)
             S_hat = svd_inv(Ka.T.dot(Seps_inv).dot(Ka) + Sa_inv,
                             hashtable=self.hashtable)
+
         return S_hat, K, G
 
     def calc_Seps(self, x, meas, geom):
         """Calculate (zero-mean) measurement distribution in radiance terms.  
-        This depends on the location in the state space. This distribution is 
+
+        Depends on the location in the state space. This distribution is 
         calculated over one or more subwindows of the spectrum. Return the 
-        inverse covariance and its square root."""
+        inverse covariance and its square root.
+        """
 
         Seps = self.fm.Seps(x, meas, geom)
         wn = len(self.winidx)
         Seps_win = np.zeros((wn, wn))
         for i in range(wn):
             Seps_win[i, :] = Seps[self.winidx[i], self.winidx]
+
         return svd_inv_sqrt(Seps_win, hashtable=self.hashtable)
 
     def invert(self, meas, geom):
         """Inverts a meaurement and returns a state vector.
 
-           Parameters:
+        Parameters:
 
-             meas           - a one-D scipy vector of radiance in uW/nm/sr/cm2
-             geom           - a geometry object.
-             plot_directory - the base directory to which diagnostics are
-                                writtena preinitialized ForwardModel object
+        meas           - a one-D scipy vector of radiance in uW/nm/sr/cm2
+        geom           - a geometry object.
+        plot_directory - the base directory to which diagnostics are
+                           writtena preinitialized ForwardModel object
 
-           Returns a tuple consisting of the following:
+        Returns a tuple consisting of the following:
 
-             lamb           - the converged lambertian surface reflectance
-             path           - the converged path radiance estimate
-             mdl            - the modeled radiance estimate
-             S_hat          - the posterior covariance of the state vector
-             G              - the G matrix from the CD Rodgers 2000 formalism
-             xopt           - the converged state vector solution"""
+        lamb           - the converged lambertian surface reflectance
+        path           - the converged path radiance estimate
+        mdl            - the modeled radiance estimate
+        S_hat          - the posterior covariance of the state vector
+        G              - the G matrix from the CD Rodgers 2000 formalism
+        xopt           - the converged state vector solution
+
+        TODO: remove nested function jac, err
+        """
 
         self.lasttime = time.time()
         self.trajectory = []
@@ -175,24 +188,25 @@ class Inversion:
         # measurement noise from the instrument as well as variability due to
         # unknown variables. For speed, we will calculate it just once based
         # on the initial solution (a potential minor source of inaccuracy).
-        Seps_inv, Seps_inv_sqrt = self.calc_Seps(x0, meas, geom)
+        _, Seps_inv_sqrt = self.calc_Seps(x0, meas, geom)
 
         def jac(x):
-            """Calculate measurement Jacobian and prior Jacobians with 
-            respect to cost function. This is the derivative of cost with
-            respect to the state, commonly known as the gradient or loss
-            surface. The cost is expressed as a vector of 'residuals'
-            with respect to the prior and measurement, expressed in absolute
-            (not quadratic) terms for the solver; It is the square root of
-            the Rodgers (2000) Chi-square version. All measurement
-            distributions are calculated over subwindows of the full spectrum."""
+            """Calculate measurement Jacobian and prior Jacobians w.r.t. cost function.
+
+            This is the derivative of cost with respect to the state, commonly
+            known as the gradient or loss surface. The cost is expressed as a
+            vector of 'residuals' with respect to the prior and measurement,
+            expressed in absolute (not quadratic) terms for the solver; It is
+            the square root of the Rodgers (2000) Chi-square version. All measurement
+            distributions are calculated over subwindows of the full spectrum.
+            """
 
             # jacobian of measurment cost term WRT state vector.
             K = self.fm.K(x, geom)[self.winidx, :]
             meas_jac = Seps_inv_sqrt.dot(K)
 
             # jacobian of prior cost term with respect to state vector.
-            xa, Sa, Sa_inv, Sa_inv_sqrt = self.calc_prior(x, geom)
+            _, _, _, Sa_inv_sqrt = self.calc_prior(x, geom)
             prior_jac = Sa_inv_sqrt
 
             # The total cost vector (as presented to the solver) is the
@@ -204,12 +218,13 @@ class Inversion:
             return np.real(total_jac)
 
         def err(x):
-            """Calculate cost function expressed here in absolute (not
-            quadratic) terms for the solver, i.e., the square root of the 
-            Rodgers (2000) Chi-square version. We concatenate 'residuals'
-            due to measurment and prior terms, suitably scaled.
-            All measurement distributions are calculated over subwindows 
-            of the full spectrum."""
+            """Calculate cost function in absolute (not quadratic) terms for solver.
+
+            i.e., the square root of the  Rodgers (2000) Chi-square version.
+            We concatenate 'residuals' due to measurment and prior terms,
+            suitably scaled. All measurement distributions are calculated over
+            subwindows of the full spectrum.
+            """
 
             # Measurement cost term.  Will calculate reflectance and Ls from
             # the state vector.
@@ -219,7 +234,7 @@ class Inversion:
             meas_resid = (est_meas_window-meas_window).dot(Seps_inv_sqrt)
 
             # Prior cost term
-            xa, Sa, Sa_inv, Sa_inv_sqrt = self.calc_prior(x, geom)
+            xa, _, _, Sa_inv_sqrt = self.calc_prior(x, geom)
             prior_resid = (x - xa).dot(Sa_inv_sqrt)
 
             # Total cost
@@ -227,13 +242,13 @@ class Inversion:
 
             # How long since the last call?
             newtime = time.time()
-            secs = newtime-self.lasttime
+            # secs = newtime-self.lasttime
             self.lasttime = newtime
 
             self.trajectory.append(x)
 
             it = len(self.trajectory)
-            tm = newtime - self.lasttime
+            # tm = newtime - self.lasttime
             rs = sum(pow(total_resid, 2))
             sm = self.fm.summarize(x, geom)
             logging.debug('Iteration: %02i  Residual: %12.2f %s' %
@@ -247,6 +262,7 @@ class Inversion:
             self.trajectory.append(xopt.x)
         except LinAlgError:
             logging.warning('Levenberg Marquardt failed to converge')
+
         return np.array(self.trajectory)
 
     def forward_uncertainty(self, x, meas, geom):
@@ -260,4 +276,5 @@ class Inversion:
         mdl = self.fm.calc_meas(x, geom, rfl=None, Ls=None)
         lamb = self.fm.calc_lamb(x, geom)
         S_hat, K, G = self.calc_posterior(x, geom, meas)
+
         return lamb, mdl, path, S_hat, K, G
