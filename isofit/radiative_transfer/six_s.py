cefd4b5920a10426e38ed258ada6dfe0625c7802
Adam Erickson <adam.michael.erickson@gmail.com>
2020-04-23 11:35:41 -0400

    switch from scipy to numpy, fix imports, reformat

--- isofit/radiative_transfer/six_s.py
+++ isofit/radiative_transfer/six_s.py
@@ -20,17 +20,17 @@
 import os
 import logging
 from datetime import datetime
-import scipy as s
+import numpy as np
 
-from isofit.core.common import resample_spectrum, load_wavelen, VectorInterpolator
 from .look_up_tables import TabularRT, FileExistsError
-from isofit.core.geometry import Geometry
+from ..core.common import resample_spectrum, load_wavelen, VectorInterpolator
+from ..core.geometry import Geometry
 
 sixs_names = ['sixs_vswir']
 
 eps = 1e-5  # used for finite difference derivative calculations
 
-sixs_template = '''0 (User defined)
+sixs_template = """0 (User defined)
 {solzen} {solaz} {viewzen} {viewaz} {month} {day}
 8  (User defined H2O, O3)
 {H2OSTR}, {O3}
@@ -50,7 +50,7 @@ sixs_template = '''0 (User defined)
 0
 0
 -1 No atm. corrections selected
-'''
+"""
 
 
 class SixSRT(TabularRT):
@@ -61,21 +61,24 @@ class SixSRT(TabularRT):
         TabularRT.__init__(self, config)
         self.sixs_dir = self.find_basedir(config)
         self.wl, self.fwhm = load_wavelen(config['wavelength_file'])
-        self.sixs_grid_init = s.arange(self.wl[0], self.wl[-1]+2.5, 2.5)
+        self.sixs_grid_init = np.arange(self.wl[0], self.wl[-1]+2.5, 2.5)
         self.sixs_ngrid_init = len(self.sixs_grid_init)
         self.sixs_dir = self.find_basedir(config)
-        self.params = {'aermodel': 1,
-                       'AOT550': 0.01,
-                       'H2OSTR': 0,
-                       'O3': 0.40,
-                       'day':   config['day'],
-                       'month': config['month'],
-                       'elev':  config['elev'],
-                       'alt':   config['alt'],
-                       'atm_file': None,
-                       'abscf_data_directory': None,
-                       'wlinf': self.sixs_grid_init[0]/1000.0,  # convert to nm
-                       'wlsup': self.sixs_grid_init[-1]/1000.0}
+
+        self.params = {
+            'aermodel': 1,
+            'AOT550': 0.01,
+            'H2OSTR': 0,
+            'O3': 0.40,
+            'day':   config['day'],
+            'month': config['month'],
+            'elev':  config['elev'],
+            'alt':   config['alt'],
+            'atm_file': None,
+            'abscf_data_directory': None,
+            'wlinf': self.sixs_grid_init[0]/1000.0,  # convert to nm
+            'wlsup': self.sixs_grid_init[-1]/1000.0
+        }
 
         if 'obs_file' in config:
             # A special case where we load the observation geometry
@@ -91,33 +94,32 @@ class SixSRT(TabularRT):
             for f in ['solzen', 'viewzen', 'solaz', 'viewaz']:
                 self.params[f] = config[f]
 
-        self.esd = s.loadtxt(config['earth_sun_distance_file'])
+        self.esd = np.loadtxt(config['earth_sun_distance_file'])
         dt = datetime(2000, self.params['month'], self.params['day'])
         self.day_of_year = dt.timetuple().tm_yday
         self.irr_factor = self.esd[self.day_of_year-1, 1]
 
-        irr = s.loadtxt(config['irradiance_file'], comments='#')
+        irr = np.loadtxt(config['irradiance_file'], comments='#')
         iwl, irr = irr.T
         irr = irr / 10.0  # convert, uW/nm/cm2
         irr = irr / self.irr_factor**2  # consider solar distance
         self.solar_irr = resample_spectrum(irr, iwl,  self.wl, self.fwhm)
 
-        self.angular_lut_keys_degrees = ['OBSZEN','TRUEAZ','viewzen','viewaz',
-            'solzen','solaz']
+        self.angular_lut_keys_degrees = ['OBSZEN', 'TRUEAZ', 'viewzen', 'viewaz',
+                                         'solzen', 'solaz']
 
         self.lut_quantities = ['rhoatm', 'transm', 'sphalb', 'transup']
         self.build_lut()
 
         # This array is used to handle the potential indexing mismatch between
-        # the 'global statevector' (which may couple multiple radiative 
+        # the 'global statevector' (which may couple multiple radiative
         # transform models) and this statevector. It should never be modified
         full_to_local_statevector_position_mapping = []
         for sn in self.statevec:
             ix = statevector_names.index(sn)
             full_to_local_statevector_position_mapping.append(ix)
         self._full_to_local_statevector_position_mapping = \
-            s.array(full_to_local_statevector_position_mapping)
-
+            np.array(full_to_local_statevector_position_mapping)
 
     def find_basedir(self, config):
         """Seek out a sixs base directory."""
@@ -178,7 +180,7 @@ class SixSRT(TabularRT):
         with open(os.path.join(self.lut_dir, 'LUT_'+fn+'.inp'), 'r') as l:
             inlines = l.readlines()
             solzen = float(inlines[1].strip().split()[0])
-        self.coszen = s.cos(solzen/360*2.0*s.pi)
+        self.coszen = np.cos(solzen/360*2.0*np.pi)
 
         # Strip header
         for i, ln in enumerate(lines):
@@ -186,12 +188,12 @@ class SixSRT(TabularRT):
                 lines = lines[(i + 1):(i + 1 + self.sixs_ngrid_init)]
                 break
 
-        solzens = s.zeros(len(lines))
-        sphalbs = s.zeros(len(lines))
-        transups = s.zeros(len(lines))
-        transms = s.zeros(len(lines))
-        rhoatms = s.zeros(len(lines))
-        self.grid = s.zeros(len(lines))
+        solzens = np.zeros(len(lines))
+        sphalbs = np.zeros(len(lines))
+        transups = np.zeros(len(lines))
+        transms = np.zeros(len(lines))
+        rhoatms = np.zeros(len(lines))
+        self.grid = np.zeros(len(lines))
 
         for i, ln in enumerate(lines):
             ln = ln.replace('*', ' ').strip()
@@ -206,24 +208,25 @@ class SixSRT(TabularRT):
             rhoatms[i] = float(rhoa)
 
         results = {
-            "solzen": resample_spectrum(solzens,  self.grid, self.wl, 
-                    self.fwhm),
-            "rhoatm": resample_spectrum(rhoatms,  self.grid, self.wl, 
-                    self.fwhm),
-            "transm": resample_spectrum(transms,  self.grid, self.wl, 
-                    self.fwhm),
-            "sphalb": resample_spectrum(sphalbs,  self.grid, self.wl, 
-                    self.fwhm),
-            "transup": resample_spectrum(transups, self.grid, self.wl, 
-                    self.fwhm)}
+            "solzen": resample_spectrum(solzens,  self.grid, self.wl,
+                                        self.fwhm),
+            "rhoatm": resample_spectrum(rhoatms,  self.grid, self.wl,
+                                        self.fwhm),
+            "transm": resample_spectrum(transms,  self.grid, self.wl,
+                                        self.fwhm),
+            "sphalb": resample_spectrum(sphalbs,  self.grid, self.wl,
+                                        self.fwhm),
+            "transup": resample_spectrum(transups, self.grid, self.wl,
+                                         self.fwhm)}
         return results
 
     def ext550_to_vis(self, ext550):
         """."""
 
-        return s.log(50.0) / (ext550 + 0.01159)
+        return np.log(50.0) / (ext550 + 0.01159)
 
     def build_lut(self, rebuild=False):
+        """."""
 
         TabularRT.build_lut(self, rebuild)
 
@@ -234,24 +237,28 @@ class SixSRT(TabularRT):
         self.cache = {}
         dims_aug = self.lut_dims + [self.n_chan]
         for key in self.lut_quantities:
-            temp = s.zeros(dims_aug, dtype=float)
+            temp = np.zeros(dims_aug, dtype=float)
             for sixs_output, point in zip(sixs_outputs, self.points):
-                ind = [s.where(g == p)[0] for g, p in \
-                    zip(self.lut_grids, point)]
+                ind = [np.where(g == p)[0] for g, p in
+                       zip(self.lut_grids, point)]
                 ind = tuple(ind)
                 temp[ind] = sixs_output[key]
 
-            self.luts[key] = VectorInterpolator(self.lut_grids, temp, 
-                    self.lut_interp_types)
+            self.luts[key] = VectorInterpolator(self.lut_grids, temp,
+                                                self.lut_interp_types)
 
     def _lookup_lut(self, point):
+        """."""
+
         ret = {}
         for key, lut in self.luts.items():
-            ret[key] = s.array(lut(point)).ravel()
+            ret[key] = np.array(lut(point)).ravel()
         return ret
 
     def get(self, x_RT, geom):
-        point = s.zeros((self.n_point,))
+        """."""
+
+        point = np.zeros((self.n_point,))
         for point_ind, name in enumerate(self.lut_grid_config):
             if name in self.statevec:
                 ix = self.statevec.index(name)
@@ -283,17 +290,22 @@ class SixSRT(TabularRT):
         return self._lookup_lut(point)
 
     def get_L_atm(self, x_RT, geom):
+        """."""
+
         r = self.get(x_RT, geom)
         rho = r['rhoatm']
-        rdn = rho / s.pi*(self.solar_irr * self.coszen)
+        rdn = rho / np.pi*(self.solar_irr * self.coszen)
         return rdn
 
     def get_L_down_transmitted(self, x_RT, geom):
+        """."""
+
         r = self.get(x_RT, geom)
-        rdn = (self.solar_irr * self.coszen) / s.pi * r['transm']
+        rdn = (self.solar_irr * self.coszen) / np.pi * r['transm']
         return rdn
 
     def get_L_up(self, x_RT, geom):
         """Thermal emission from the ground is provided by the thermal model, so
         this function is a placeholder for future upgrades."""
+
         return 0
