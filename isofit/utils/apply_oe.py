1e7c3680d12080b53821c76713de5483a2442900
David R Thompson <davidraythompson@gmail.com>
2020-12-14 04:51:08 -0800

    Aerosol LUT grid rounding fix, ftol discussion (#192)
    Corrects the lower bound rounding for aerosol LUT grids in apply_oe.py
    
    Co-authored-by: David R Thompson <david.r.thompson@jpl.nasa.gov>

--- isofit/utils/apply_oe.py
+++ isofit/utils/apply_oe.py
@@ -1201,13 +1201,15 @@ def build_main_config(paths: Pathnames, lut_params: LUTConfig, h2o_lut_grid: np.
         grid = {}
         if h2o_lut_grid is not None:
             h2o_delta = float(h2o_lut_grid[-1]) - float(h2o_lut_grid[0])
-            grid['H2OSTR'] = [round(h2o_lut_grid[0]+h2o_delta*0.02,4), round(h2o_lut_grid[-1]-h2o_delta*0.02,4)]
+            grid['H2OSTR'] = [round(h2o_lut_grid[0]+h2o_delta*0.02,4), 
+                              round(h2o_lut_grid[-1]-h2o_delta*0.02,4)]
 
         # We will initialize using different AODs for the first aerosol in the LUT
         if len(aerosol_lut_grid)>0:
             key = list(aerosol_lut_grid.keys())[0]
             aer_delta = aerosol_lut_grid[key][-1] - aerosol_lut_grid[key][0]
-            grid[key] = [round(aerosol_lut_grid[key][0]+aer_delta*0.02), round(aerosol_lut_grid[key][-1]-aer_delta*0.02,4)]
+            grid[key] = [round(aerosol_lut_grid[key][0]+aer_delta*0.02,4), 
+                         round(aerosol_lut_grid[key][-1]-aer_delta*0.02,4)]
         isofit_config_modtran['implementation']['inversion']['integration_grid'] = grid
         isofit_config_modtran['implementation']['inversion']['inversion_grid_as_preseed'] = True
 
