51efd2f842be19dccaddde12cfb570d1df45444c
Winston Olson-Duvall <winston.olson-duvall@jpl.nasa.gov>
2021-03-15 15:33:41 -0700

    Clone system-test-2-v2 to get working version and then add all the system-test-3 files to allow for integration with EcoSIS using new surface.json file.

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -1,83 +1,64 @@
 #!/bin/bash
 
 # Get directories and paths for scripts
-basedir=$( cd "$(dirname "$0")" ; pwd -P )
-isofit_dir=$(dirname $basedir)
+imgspec_dir=$( cd "$(dirname "$0")" ; pwd -P )
+isofit_dir=$(dirname $imgspec_dir)
 
-echo "basedir is $basedir"
+echo "imgspec_dir is $imgspec_dir"
 echo "isofit_dir is $isofit_dir"
 
+# input/output dirs
+input="input"
+mkdir -p output
+
 # .imgspec paths
-apply_glt_exe="$isofit_dir/.imgspec/apply_glt.py"
-wavelength_file_exe="$isofit_dir/.imgspec/wavelength_file.py"
-basic_surface_json_path="$isofit_dir/.imgspec/basic_surface.json"
+apply_glt_exe="$imgspec_dir/apply_glt.py"
+wavelength_file_exe="$imgspec_dir/wavelength_file.py"
+covnert_csv_to_envi_exe="$imgspec_dir/convert_csv_to_envi.py"
+surface_json_path="$imgspec_dir/surface.json"
 
 # utils paths
 surface_model_exe="$isofit_dir/isofit/utils/surface_model.py"
 apply_oe_exe="$isofit_dir/isofit/utils/apply_oe.py"
 
-# data path
-input_spectrum_files_path="$isofit_dir/data/reflectance/surface_model_ucsb"
+# ecosis input spectra paths
+filtered_other_csv_path="$input/surface-reflectance-spectra.csv"
+filtered_veg_csv_path="$input/vegetation_reflectance_spectra.csv"
+filtered_ocean_csv_path="$input/water_reflectance_spectra.csv"
+surface_liquids_csv_path="$input/snow_and_liquids_reflectance_spectra.csv"
 
-# input/output dirs
-input="input"
-mkdir output
+# Process positional args to get EcoSIS CSV files
+#wget  --directory-prefix input --content-disposition $1
+wget -O $filtered_other_csv_path $1
+wget -O $filtered_veg_csv_path $2
+wget -O $filtered_ocean_csv_path $3
+wget -O $surface_liquids_csv_path $4
+
+# Converted spectra ENVI paths
+filtered_other_img_path=${filtered_other_csv_path/.csv/}
+filtered_veg_img_path=${filtered_veg_csv_path/.csv/}
+filtered_ocean_img_path=${filtered_ocean_csv_path/.csv/}
+surface_liquids_img_path=${surface_liquids_csv_path/.csv/}
+
+# Get input paths
+rdn_path=$(python ${imgspec_dir}/get_paths_from_granules.py -p rdn)
+echo "Found input radiance file: $rdn_path"
+loc_path=$(python ${imgspec_dir}/get_paths_from_granules.py -p loc)
+echo "Found input loc file: $loc_path"
+glt_path=$(python ${imgspec_dir}/get_paths_from_granules.py -p glt)
+echo "Found input glt file: $glt_path"
+obs_ort_path=$(python ${imgspec_dir}/get_paths_from_granules.py -p obs_ort)
+echo "Found input obs_ort file: $obs_ort_path"
 
 # Get instrument type
-gzip_path=$(ls $input/*gz)
-echo "Found input gzip file: $gzip_path"
-gzip_file=$(basename $gzip_path)
+rdn_name=$(basename $rdn_path)
 instrument=""
-if [[ $gzip_file == f* ]]; then
-    instrument="AVCL"
-elif [[ $gzip_file == ang* ]]; then
-    instrument="AVNG"
+if [[ $rdn_name == f* ]]; then
+    instrument="avcl"
+elif [[ $rdn_name == ang* ]]; then
+    instrument="ang"
 fi
 echo "Instrument is $instrument"
-
-# Unzip file and remove gzip
-echo "Unzipping files..."
-for file in $input/*tar.gz; do
-    tar xzvf $file --directory $input/
-#    rm -f $file
-done
-
-# Get rdn, loc, and obs based on instrument type
-if [[ $instrument == "AVNG" ]]; then
-    rdn_path=$(ls $input/ang*rdn*/ang*rdn*img)
-    if [[ -f $rdn_path ]]; then
-        echo "Found rdn file $rdn_path"
-    else
-        echo "Couldn't find rdn file, exiting..."
-        exit 1
-    fi
-
-    loc_path=$(ls $input/ang*rdn*/ang*rdn*loc)
-    if [[ -f $loc_path ]]; then
-        echo "Found loc file $loc_path"
-    else
-        echo "Couldn't find loc file, exiting..."
-        exit 1
-    fi
-
-    glt_path=$(ls $input/ang*rdn*/ang*rdn*glt)
-    if [[ -f $glt_path ]]; then
-        echo "Found glt file $glt_path"
-    else
-        echo "Couldn't find glt file, exiting..."
-        exit 1
-    fi
-
-    obs_ort_path=$(ls $input/ang*rdn*/ang*rdn*obs_ort)
-    if [[ -f $obs_ort_path ]]; then
-        echo "Found obs_ort file $obs_ort_path"
-    else
-        echo "Couldn't find obs_ort file, exiting..."
-        exit 1
-    fi
-fi
-
-# TODO: Add block for AVCL files
 
 # Create wavelength file
 wavelength_file_cmd="python $wavelength_file_exe $rdn_path.hdr $input/wavelengths.txt"
@@ -91,19 +72,39 @@ apply_glt_cmd="python $apply_glt_exe $loc_path $glt_path $loc_ort_path"
 echo "Executing command: $apply_glt_cmd"
 $apply_glt_cmd
 
-# Build surface model based on basic_surface.json template
-sed -e "s|\${output_model_file}|\./basic_surface.mat|g" \
+# Build surface model based on surface.json template and input spectra CSV
+# First convert CSV to ENVI for 3 spectra files
+convert_csv_to_envi_cmd="python $covnert_csv_to_envi_exe $filtered_other_csv_path"
+echo "Executing command: convert_csv_to_envi_cmd on $filtered_other_csv_path"
+$convert_csv_to_envi_cmd
+
+convert_csv_to_envi_cmd="python $covnert_csv_to_envi_exe $filtered_veg_csv_path"
+echo "Executing command: convert_csv_to_envi_cmd on $filtered_veg_csv_path"
+$convert_csv_to_envi_cmd
+
+convert_csv_to_envi_cmd="python $covnert_csv_to_envi_exe $filtered_ocean_csv_path"
+echo "Executing command: convert_csv_to_envi_cmd on $filtered_ocean_csv_path"
+$convert_csv_to_envi_cmd
+
+convert_csv_to_envi_cmd="python $covnert_csv_to_envi_exe $surface_liquids_csv_path"
+echo "Executing command: convert_csv_to_envi_cmd on $surface_liquids_csv_path"
+$convert_csv_to_envi_cmd
+
+sed -e "s|\${output_model_file}|\./surface.mat|g" \
     -e "s|\${wavelength_file}|\./wavelengths.txt|g" \
-    -e "s|\${input_spectrum_files}|$input_spectrum_files_path|g" \
-    $basic_surface_json_path > $input/basic_surface.json
-echo "Building surface model using config file $input/basic_surface.json"
-python -c "from isofit.utils import surface_model; surface_model('$input/basic_surface.json')"
+    -e "s|\${input_spectrum_filtered_other}|${filtered_other_img_path/input/\.}|g" \
+    -e "s|\${input_spectrum_filtered_veg}|${filtered_veg_img_path/input/\.}|g" \
+    -e "s|\${input_spectrum_filtered_ocean}|${filtered_ocean_img_path/input/\.}|g" \
+    -e "s|\${input_spectrum_surface_liquids}|${surface_liquids_img_path/input/\.}|g" \
+    $surface_json_path > $input/surface.json
+echo "Building surface model using config file $input/surface.json"
+python -c "from isofit.utils import surface_model; surface_model('$input/surface.json')"
 
 # Run isofit
 working_dir=$(pwd)
-isofit_cmd="""python $apply_oe_exe $rdn_path $loc_ort_path $obs_ort_path $working_dir ang --presolve=1 \
+isofit_cmd="""python $apply_oe_exe $rdn_path $loc_ort_path $obs_ort_path $working_dir $instrument --presolve=1 \
 --empirical_line=1 --emulator_base=$EMULATOR_DIR --n_cores 30 --wavelength_path $input/wavelengths.txt \
---surface_path $input/basic_surface.mat --log_file isofit.log"""
+--surface_path $input/surface.mat --log_file isofit.log"""
 echo "Executing command: $isofit_cmd"
 $isofit_cmd
 
