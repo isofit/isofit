45b597e58a27b29490cdfc50c7fd0d8ae0138e03
varunpatro <vpatro@ndc.nasa.gov>
2022-06-23 13:05:34 -0700

    partial test case for emissive_radiance()

--- isofit/test/prelim_common.py
+++ isofit/test/prelim_common.py
@@ -20,6 +20,34 @@ def load_wavelen(wavelength_file: str):
     wl, fwhm = q.T
     return wl, fwhm
 
+def emissive_radiance(emissivity: np.array, T: np.array, wl: np.array) -> (np.array, np.array):
+    """Calcluate the radiance of a surface due to emission.
+
+    Args:
+        emissivity: surface emissivity.
+        T: surface temperature [K]
+        wl: emmissivity wavelengths [nm]
+
+    Returns:
+        np.array: surface upwelling radiance in uW $cm^{-2} sr^{-1} nm^{-nm}$
+        np.array: partial derivative of radiance with respect to temperature uW $cm^{-2} sr^{-1} nm^{-1} k^{-1}$
+
+    """
+
+    c_1 = 1.88365e32/np.pi
+    c_2 = 14387690
+    J_per_eV = 1.60218e-19
+    wl_um = wl / 1000.0
+    ph_per_sec_cm2_sr_nm = c_1/(wl**4)/(np.exp(c_2/wl/T)-1.0) * emissivity
+    # photon energy in eV
+    eV_per_sec_cm2_sr_nm = 1.2398 * ph_per_sec_cm2_sr_nm/wl_um
+    W_per_cm2_sr_nm = J_per_eV * eV_per_sec_cm2_sr_nm
+    uW_per_cm2_sr_nm = W_per_cm2_sr_nm*1e6
+    dRdn_dT = c_1/(wl**4)*(-pow(np.exp(c_2/wl/T)-1.0, -2.0)) *\
+        np.exp(c_2/wl/T)*(-pow(T, -2)*c_2/wl) *\
+        emissivity/wl_um*1.2398*J_per_eV*1e6
+    return uW_per_cm2_sr_nm, dRdn_dT
+
 
 
 print("BEGIN")
@@ -47,23 +75,18 @@ for i in myList:
 #assert(wl_modified[0] > 100)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #wl_modified, fwhm_modified = load_wavelen("C:/Users/vpatro/Desktop/Test/wl_multicol.txt")
 #print(wl_modified.shape)
 
+emissivity = np.array([0.8, 0.8])
+T = np.array([300, 300])
+wavelength = np.array([400, 600])
+
+#uW_per_cm2_sr_nm_modified, dRdn_dT_modified = emissive_radiance(emissivity, T, wavelength)
+#print(uW_per_cm2_sr_nm_modified)
+#print(dRdn_dT_modified)
+
+print(pow(4,-2))
     
 
 
