2708f651cf7ebac83e1cd501e87e9a2b58b8ff02
unbohn <nbohn.1988@googlemail.com>
2023-01-30 10:34:12 -0800

    Added run_small.sh and run_medium.sh and adjusted both to multisurface approach.

--- examples/image_cube/run_small.sh
+++ examples/image_cube/run_small.sh
@@ -13,8 +13,8 @@ file_base=ang20170323t202244
 
 # Adjust a few config settings
 jq '.general_options.empirical_line=$newVal' --argjson newVal false configs/basic_config.json > tmp.$$.json && mv tmp.$$.json configs/basic_config.json
-jq '.general_options.debug_mode=$newVal' --argjson newVal true configs/basic_config.json > tmp.$$.json && mv tmp.$$.json configs/basic_config.json
+jq '.general_options.debug_mode=$newVal' --argjson newVal false configs/basic_config.json > tmp.$$.json && mv tmp.$$.json configs/basic_config.json
 jq '.processors.general_inversion_parameters.filepaths.emulator_base=env.EMULATOR_PATH' configs/basic_config.json >> tmp.$$.json && mv tmp.$$.json configs/basic_config.json
 
 # Small test (10x10 pixels, no empirical line) - this should take 4-5 minutes with n_cores = 4.
-python -m cProfile -o medium_profile.dat "${isofit_base_path}"/utils/multisurface_oe.py small_chunk/${file_base}_rdn_7000-7010 small_chunk/${file_base}_loc_7000-7010 small_chunk/${file_base}_obs_7000-7010 small_chunk_test configs/basic_config.json
+python "${isofit_base_path}"/utils/multisurface_oe.py small_chunk/${file_base}_rdn_7000-7010 small_chunk/${file_base}_loc_7000-7010 small_chunk/${file_base}_obs_7000-7010 small_chunk_test configs/basic_config.json
