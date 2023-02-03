1ce934b5ff8f8df00d3276551b998f4f6251d3a3
James Montgomery <github@booms.dev>
2023-02-03 11:03:56 -0800

    Cleaned up unneeded files

--- examples/profiling_cube/run.sh
+++ examples/profiling_cube/run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Download relevant datasets - unlike other examples, these datasets are too large to place in
+if test -f "test_data_rev.zip"; then
+  echo "Test zip already present, skipping download"
+else
+  curl -O https://avng.jpl.nasa.gov/pub/PBrodrick/isofit/test_data_rev.zip
+  unzip test_data_rev.zip
+  rm -r medium_chunk
+fi
+
+isofit_base_path=$(python -c "import isofit; import os; print(os.path.dirname(isofit.__file__))")
+file_base=ang20170323t202244
+
 for i in {0..4}; do
   echo "Running $i/5 and saving to $1.$i.dat"
 
