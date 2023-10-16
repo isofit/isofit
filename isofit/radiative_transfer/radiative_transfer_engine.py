28eabef6a20f837fe57ef363d76dab66f91d019e
unbohn <nbohn.1988@googlemail.com>
2023-10-16 15:38:59 -0700

    Moved two_albedo_method back to radiative_transfer_engine.py.

--- isofit/radiative_transfer/radiative_transfer_engine.py
+++ isofit/radiative_transfer/radiative_transfer_engine.py
@@ -361,6 +361,125 @@ class RadiativeTransferEngine:
         pairs = zip(self.lut_grid.keys(), x_RT)
         return " ".join([f"{name}={val:5.3f}" for name, val in pairs])
 
+    # ToDo: we need to think about the best place for the two albedo method (here, radiative_transfer.py, utils, etc.)
+    @staticmethod
+    def two_albedo_method(
+            p0: dict,
+            p1: dict,
+            p2: dict,
+            coszen: float,
+            rfl1: float = 0.1,
+            rfl2: float = 0.5,
+    ) -> dict:
+        """
+        Calculates split transmittance values from a multipart file using the
+        two-albedo method. See notes for further detail
+
+        Parameters
+        ----------
+        p0: dict
+            Part 0 of the channel file
+        p1: dict
+            Part 1 of the channel file
+        p2: dict
+            Part 2 of the channel file
+        coszen: float
+            ...
+        rfl1: float, defaults=0.1
+            Reflectance scaler 1
+        rfl2: float, defaults=0.5
+            Reflectance scaler 2
+
+        Returns
+        -------
+        data: dict
+            Relevant information
+
+        Notes
+        -----
+        This implementation follows Guanter et al. (2009)
+        (DOI:10.1080/01431160802438555), modified by Nimrod Carmon. It is called
+        the "2-albedo" method, referring to running MODTRAN with 2 different
+        surface albedos. Alternatively, one could also run the 3-albedo method,
+        which is similar to this one with the single difference where the
+        "path_radiance_no_surface" variable is taken from a
+        zero-surface-reflectance MODTRAN run instead of being calculated from
+        2 MODTRAN outputs.
+
+        There are a few argument as to why the 2- or 3-albedo methods are
+        beneficial:
+            (1) For each grid point on the lookup table you sample MODTRAN 2 or
+                3 times, i.e., you get 2 or 3 "data points" for the atmospheric
+                parameter of interest. This in theory allows us to use a lower
+                band model resolution for the MODTRAN run, which is much faster,
+                while keeping high accuracy.
+            (2) We use the decoupled transmittance products to expand
+                the forward model and account for more physics, currently
+                topography and glint.
+        """
+        # Extract relevant columns
+        widths = p0["width"]
+        t_up_dirs = p0["transup"]
+
+        # REVIEW: two_albedo_method-v1 used a single solar_irr value, but now we have an array of values
+        # The last value in the new array is the same as the old v1, so for backwards compatibility setting that here
+        toa_irad = p0["solar_irr"][-1] * coszen / np.pi
+
+        # Calculate some fluxes
+        directRflt1 = p1["drct_rflt"]
+        groundRflt1 = p1["grnd_rflt"]
+
+        directFlux1 = directRflt1 * np.pi / rfl1 / t_up_dirs
+        globalFlux1 = groundRflt1 * np.pi / rfl1 / t_up_dirs
+
+        # diffuseFlux = globalFlux1 - directFlux1 # Unused
+
+        globalFlux2 = p2["grnd_rflt"] * np.pi / rfl2 / t_up_dirs
+
+        # Path radiances
+        rdn1 = p1["path_rdn"]
+        rdn2 = p2["path_rdn"]
+
+        # Path Radiance No Surface = prns
+        val1 = rfl1 * globalFlux1  # TODO: Needs a better name
+        val2 = rfl2 * globalFlux2  # TODO: Needs a better name
+        prns = ((val2 * rdn1) - (val1 * rdn2)) / (val2 - val1)
+
+        # Diffuse upwelling transmittance
+        t_up_difs = np.pi * (rdn1 - prns) / (rfl1 * globalFlux1)
+
+        # Spherical Albedo
+        sphalbs = (globalFlux1 - globalFlux2) / (val1 - val2)
+        dFluxRN = directFlux1 / coszen  # Direct Flux Radiance
+
+        globalFluxNS = globalFlux1 * (1 - rfl1 * sphalbs)  # Global Flux No Surface
+        diffusFluxNS = globalFluxNS - dFluxRN * coszen  # Diffused Flux No Surface
+
+        t_down_dirs = dFluxRN * coszen / widths / np.pi / toa_irad
+        t_down_difs = diffusFluxNS / widths / np.pi / toa_irad
+
+        transms = (t_down_dirs + t_down_difs) * (t_up_dirs + t_up_difs)
+
+        # Return some keys from the first part plus the new calculated keys
+        pass_forward = [
+            "wl",
+            "rhoatm",
+            "solar_irr",
+            "thermal_upwelling",
+            "thermal_downwelling",
+        ]
+        data = {
+            "sphalb": sphalbs,
+            "transm_up_dir": t_up_dirs,
+            "transm_up_dif": t_up_difs,
+            "transm_down_dir": t_down_dirs,
+            "transm_down_dif": t_down_difs,
+        }
+        for key in pass_forward:
+            data[key] = p0[key]
+
+        return data
+
 
 @ray.remote
 def stream_simulation(
