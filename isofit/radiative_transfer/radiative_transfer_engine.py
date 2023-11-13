6c066b14848c1e2feb90acd48d2a72b86286b942
James Montgomery <github@booms.dev>
2023-11-13 14:05:20 -0800

    Fixed checking if override values are provided

--- isofit/radiative_transfer/radiative_transfer_engine.py
+++ isofit/radiative_transfer/radiative_transfer_engine.py
@@ -174,7 +174,7 @@ class RadiativeTransferEngine:
 
             # If the parameters aren't provided, use the wavelengths file
             _wl, _fwhm = wl, fwhm
-            if [] in (wl, fwhm):
+            if not all([len(wl), len(fwhm)]):
                 Logger.debug(
                     f"WL or FWHM were not provided, using wavelength file: {self.wavelength_file}"
                 )
