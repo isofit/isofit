604765060e7b020388a7878ae8faef684dd80679
Kevin Wurster <kevin.wurster@planet.com>
2024-02-27 11:40:52 -0500

    Reach into venv instead of activating
    A virtual environment does not need to be active in order to be
    modified. This is much simpler than attempting to activate the
    environment.

--- scripts/setup-devenv.sh
+++ scripts/setup-devenv.sh
@@ -33,27 +33,22 @@ fi
 # Create virtual environment.
 python3 -m venv "${VENV_PATH}"
 
-# Activate virtual environment. SUPER CRITICAL that this activation happens.
-# Some shell scripts assume they are in an isolated environment.
-source "${VENV_PATH}/bin/activate"
-if [ "${VIRTUAL_ENV}" == "" ]; then
-  echo "ERROR: Virtual environment did not activate: ${VENV_PATH}"
-  exit 1
-fi
+# The version of Python inside of the virtual environment is isolated, and can
+# be used to install packages inside of the environment without activating it.
+PYTHON="${VENV_PATH}/bin/python3"
 
 # Useful for debugging
-which python3
-python3 --version
+"${PYTHON}" --version
 
 # Install and upgrade packaging dependencies. The presence of 'wheel' triggers
 # a lot of the emerging Python packaging ecosystem changes (PEP-517).
-python3 -m pip install setuptools wheel --upgrade
+"${PYTHON}" -m pip install pip setuptools wheel --upgrade
 
-# Install ISOFIT.
-python3 -m pip install -e ".[dev,test]"
+# Install ISOFIT and dependencies in editable mode.
+"${PYTHON}" -m pip install -e ".[dev,test]"
 
 # Install commit hooks.
-python3 -m pre_commit install
+"${PYTHON}" -m pre_commit install
 
 # Download and unpack additional dependencies.
 "./${SCRIPT_DIR}/download-and-unpack-sRTMnet.sh"
