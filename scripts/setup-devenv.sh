a4c4aa4bdba29d81a4dc366d83661df6a7fe535b
Kevin Wurster <kevin.wurster@planet.com>
2024-02-28 11:06:24 -0500

    Backport updates to 'setup-devenv.sh'

--- scripts/setup-devenv.sh
+++ scripts/setup-devenv.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-# This script serves as a reference for how to set up a developer environment.
-# Some packages, like GDAL, must be installed prior to running this script.
-# On Ubuntu these packages are likely installed via '$ apt' or '$ apt-get',
-# and on MacOS Homebrew is a common choice. Developers should read through
-# this script, and the other scripts it invokes, and determine if it is
-# appropriate to execute for their environment. At a high-level the steps
-# should be matched, but the specific commands may need to be modified.
+# This script sets up, and refreshes, a development environment. Users must
+# create a virtual environment, and then point this script to it like:
+#
+#     $ python3.11 -m venv venv
+#     $ ./scripts/setup-devenv.sh venv
+#
+# Note that different developers may use different versions of Python. Some
+# environments may require installing additional non-Python dependencies using
+# an appropriate package manage.
 
 
 set -x
@@ -16,44 +18,36 @@ set -o pipefail
 set -o nounset
 
 
-VENV_PATH="venv"
-
-
-# Path to directory containing this script.
+# Path to directory containing this script. Needed to determine the location of
+# other scripts required for setup.
 SCRIPT_DIR=$(dirname "${0}")
 
 
-# Do not attempt to modify an existing virtual environment. We do not know what
-# it contains.
-if [ -d "${VENV_PATH}" ]; then
-  echo "Virtual environment already exists: ${VENV_PATH}"
-  exit 0
-fi
-
-# Create virtual environment.
-python3 -m venv "${VENV_PATH}"
-
-# Activate virtual environment. SUPER CRITICAL that this activation happens.
-# Some shell scripts assume they are in an isolated environment.
-source "${VENV_PATH}/bin/activate"
-if [ "${VIRTUAL_ENV}" == "" ]; then
-  echo "ERROR: Virtual environment did not activate: ${VENV_PATH}"
+# Parse arguments.
+if [ $# -eq 1 ]; then
+  VENV_PATH="$1"
+else
+  echo "Usage: ${SCRIPT_DIR}/$(basename "$0") path/to/venv"
   exit 1
 fi
 
+
+# The version of Python inside of the virtual environment is isolated, and can
+# be used to install packages inside of the environment without activating it.
+PYTHON="${VENV_PATH}/bin/python3"
+
 # Useful for debugging
-which python3
-python3 --version
+"${PYTHON}" --version
 
 # Install and upgrade packaging dependencies. The presence of 'wheel' triggers
 # a lot of the emerging Python packaging ecosystem changes (PEP-517).
-python3 -m pip install setuptools wheel --upgrade
+"${PYTHON}" -m pip install pip setuptools wheel --upgrade
 
-# Install ISOFIT.
-python3 -m pip install -e ".[dev]"
+# Install ISOFIT and dependencies in editable mode.
+"${PYTHON}" -m pip install -e ".[dev,test]"
 
 # Install commit hooks.
-python3 -m pre_commit install
+"${PYTHON}" -m pre_commit install
 
 # Download and unpack additional dependencies.
 "./${SCRIPT_DIR}/download-and-unpack-sRTMnet.sh"
