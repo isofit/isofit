285a765f56660804293117f48a1b4672c47f27c7
Phil Brodrick <phil.brodrick@gmail.com>
2024-02-29 11:19:10 -0800

    there are now 10 cli utilities

--- isofit/test/test_cli_machinery.py
+++ isofit/test/test_cli_machinery.py
@@ -40,7 +40,7 @@ def test_subcommand_registration(executable):
         assert cmd in stdout_txt
 
     # Check to make sure the right number of subcommands are registered
-    assert subcommand_count == 8
+    assert subcommand_count == 10
 
 
 def test_version():
