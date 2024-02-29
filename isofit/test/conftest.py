4274ba77f0258158317b660eee535c187b4c6067
Kevin Wurster <kevin.wurster@planet.com>
2024-02-29 14:29:07 -0500

    Fix '$ pytest -m unmarked' machinery
    Every time I look at the 'pytest' source code I kind of recoil in
    horror. It seems to be far more clever than is necessary.

--- isofit/test/conftest.py
+++ isofit/test/conftest.py
@@ -9,6 +9,19 @@ def pytest_collection_modifyitems(items, config):
     ``unmarked`` marker that can be used to select these tests.
     """
 
+    # Detecting an unmarked test is actually a bit difficult. Any marker generated with
+    # '@pytest.mark' counts as a marked test, including '@pytest.mark.parametrize()'.
+    # To get around this, we set 'addopts = --strict-markers' in 'pytest.ini' to
+    # force all markers to be registered, then we can consider any marker that is
+    # not registered to be a bulitin marker, and thus consider any test not marked
+    # with a custom marker to be 'unmarked'.
+
+    # Get registered markers from the 'pytest' configuration.
+    registered_markers = config.getini("markers")
+
     for item in items:
-        if not any(item.iter_markers()):
+        for marker in item.iter_markers():
+            if marker.name in registered_markers:
+                continue
+        else:
             item.add_marker("unmarked")
