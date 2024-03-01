37e430f8b8c22d8e5618b83d27b7348dee7dee04
unbohn <nbohn.1988@googlemail.com>
2024-03-01 12:43:58 -0800

    Removed memory_gb flag from command line calls in test_cli.py.

--- isofit/test/test_cli.py
+++ isofit/test/test_cli.py
@@ -87,9 +87,9 @@ def files(cube_example):
 
 # fmt: off
 @pytest.mark.parametrize("args", [
-    ["ang", "--presolve", "--emulator_base", EMULATOR_PATH, "--n_cores", CORES, "--memory_gb", 12, "--analytical_line", "-nn", 10, "-nn", 50,],
-    ["ang", "--presolve", "--emulator_base", EMULATOR_PATH, "--n_cores", CORES, "--memory_gb", 12, "--analytical_line", "-nn", 10, "-nn", 50, "-nn", 10, "--pressure_elevation",],
-    ["ang", "--presolve", "--emulator_base", EMULATOR_PATH, "--n_cores", CORES, "--memory_gb", 12, "--empirical_line", "--surface_category", "additive_glint_surface",],
+    ["ang", "--presolve", "--emulator_base", EMULATOR_PATH, "--n_cores", CORES, "--analytical_line", "-nn", 10, "-nn", 50,],
+    ["ang", "--presolve", "--emulator_base", EMULATOR_PATH, "--n_cores", CORES, "--analytical_line", "-nn", 10, "-nn", 50, "-nn", 10, "--pressure_elevation",],
+    ["ang", "--presolve", "--emulator_base", EMULATOR_PATH, "--n_cores", CORES, "--empirical_line", "--surface_category", "additive_glint_surface",],
 ])
 # fmt: on
 def test_apply_oe(files, args, surface):
