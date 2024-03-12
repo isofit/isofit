38fc38bf9c7243a543dc3d6bfd99b6a15d675f00
James Montgomery <github@booms.dev>
2024-03-12 15:30:24 -0700

    Added extra time to the shutdown

--- isofit/__init__.py
+++ isofit/__init__.py
@@ -45,12 +45,13 @@ else:
 
 def shutdown_ray():
     try:
-        ray.shutdown()
+        ray.shutdown(_exiting_interpreter=True)
     except:
         pass
 
 
 # Auto call ray.shutdown when the python interpreter exits
+# ray itself also implements this, but there's no harm in calling it twice
 atexit.register(shutdown_ray)
 signal.signal(signal.SIGINT, shutdown_ray)  # ctrl+C
 signal.signal(signal.SIGTERM, shutdown_ray)  # kill
