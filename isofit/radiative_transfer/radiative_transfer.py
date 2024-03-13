13ded98b2dce32a88f25019699c040e55ee9fd5f
unbohn <nbohn.1988@googlemail.com>
2024-03-12 18:36:30 -0700

    Reflectance factor for sky radiance is now calculated using the Fresnel equation for unpolarized light as a function of view zenith angle.

--- isofit/radiative_transfer/radiative_transfer.py
+++ isofit/radiative_transfer/radiative_transfer.py
@@ -180,7 +180,7 @@ class RadiativeTransfer:
             E_d = E_dd + E_ds
             L_sky = x_surface[-2] * E_dd + x_surface[-1] * E_ds
 
-            rho_ls = 0.02  # fresnel reflectance factor (approx. 0.02 for nadir view)
+            rho_ls = self.fresnel_rf(geom.observer_zenith)
             glint = rho_ls * (L_sky / E_d)
 
             ret = (
@@ -256,7 +256,7 @@ class RadiativeTransfer:
             E_d = E_dd + E_ds
             L_sky = x_surface[-2] * E_dd + x_surface[-1] * E_ds
 
-            rho_ls = 0.02  # fresnel reflectance factor (approx. 0.02 for nadir view)
+            rho_ls = self.fresnel_rf(geom.observer_zenith)
             glint = rho_ls * (L_sky / E_d)
 
             drho_scaled_for_multiscattering_drfl = (
@@ -312,6 +312,28 @@ class RadiativeTransfer:
 
         Kb_RT = np.array(Kb_RT).T
         return Kb_RT
+
+    @staticmethod
+    def fresnel_rf(vza):
+        """Calculates reflectance factor of sky radiance based on the
+        Fresnel equation for unpolarized light as a function of view zenith angle (vza).
+        """
+        if vza > 0.0:
+            n_w = 1.33  # refractive index of water
+            theta = np.deg2rad(vza)
+
+            # calculate angle of refraction using Snell′s law
+            theta_i = np.arcsin(np.sin(theta) / n_w)
+
+            # reflectance factor of sky radiance based on the Fresnel equation for unpolarized light
+            rho_s = 0.5 * np.abs(
+                np.sin(theta - theta_i) ** 2 / np.sin(theta + theta_i) ** 2
+                + np.tan(theta - theta_i) ** 2 / np.tan(theta + theta_i) ** 2
+            )
+        else:
+            rho_s = 0.02  # the reflectance factor converges to 0.02 for view angles equal to 0.0°
+
+        return rho_s
 
     def summarize(self, x_RT, geom):
         ret = []
