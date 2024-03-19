2fdf7876ea11c9b8516407a9fa539484ea3a24fe
unbohn <nbohn.1988@googlemail.com>
2024-03-19 12:00:52 -0700

    Re-added emulator aux file to download script.

--- scripts/download-and-unpack-sRTMnet.sh
+++ scripts/download-and-unpack-sRTMnet.sh
@@ -12,7 +12,8 @@ set -o nounset
 
 SRTMNET_DIR="sRTMnet_v100"
 SRTMNET_PATH="${SRTMNET_DIR}/sRTMnet_v100.h5"
-SRTMNET_FILENAME="sRTMnet_v100.h5"
+SRTMNET_MODEL_FILENAME="sRTMnet_v100.h5"
+SRTMNET_ZIPFILENAME="sRTMnet_v100.zip"
 
 # If the desired sRTM net file already exists just assume it is correct and do
 # nothing.
@@ -23,8 +24,33 @@ fi
 
 mkdir -p "${SRTMNET_DIR}"
 
-# Download sRTMnet file
+# Download sRTMnet model file
 wget \
   --no-verbose \
   --directory-prefix "${SRTMNET_DIR}" \
-  "https://avng.jpl.nasa.gov/pub/PBrodrick/isofit/${SRTMNET_FILENAME}"
+  "https://avng.jpl.nasa.gov/pub/PBrodrick/isofit/${SRTMNET_MODEL_FILENAME}"
+
+# Download sRTMnet aux file
+wget \
+   --no-verbose \
+   --directory-prefix "${SRTMNET_DIR}" \
+   "https://zenodo.org/record/4096627/files/${SRTMNET_ZIPFILENAME}"
+
+ # Unpack and delete some extraneous files.
+ #
+ # Run as a subprocess to allow 'cd'-ing into a subdirectory while automatically
+ # returning to the current working directory once the subprocess exits.
+ (
+
+   cd "${SRTMNET_DIR}"
+   unzip "${SRTMNET_DIR}"
+
+   # Remove MacOS specific files that the original author erroneously included
+   # in the archive.
+   rm -rf "__MACOSX"
+   rm -f .DS_Store sRTMnet_v100/.DS_Store
+
+   # The archive is quite large and no longer needed.
+   rm "${SRTMNET_ZIPFILENAME}"
+
+ )
