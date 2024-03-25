808ec84d212fa6eb4185131fea344c14fd31676e
unbohn <nbohn.1988@googlemail.com>
2024-03-25 16:04:21 -0700

    Rebased to dev.

--- isofit/configs/sections/radiative_transfer_config.py
+++ isofit/configs/sections/radiative_transfer_config.py
@@ -86,6 +86,11 @@ class RadiativeTransferEngineConfig(BaseConfigSection):
         implementation of the forward model.
         """
 
+        self._rt_mode = str
+        self.rt_mode = None
+        """str: Radiative transfer mode of LUT simulations. 
+        'transm' for transmittances, 'rdn' for reflected radiance."""
+
         self._glint_model_type = bool
         self.glint_model = False
         """
