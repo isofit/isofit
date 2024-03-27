afa51ab94991eedb674316b03972029644332a77
Phil Brodrick <phil.brodrick@gmail.com>
2024-03-27 12:57:24 -0700

    parallel implementation of kf

--- isofit/radiative_transfer/kernel_flows.py
+++ isofit/radiative_transfer/kernel_flows.py
@@ -23,12 +23,57 @@ import logging
 import h5py
 import numpy as np
 
+from isofit import ray
 from isofit.configs.sections.radiative_transfer_config import (
     RadiativeTransferEngineConfig,
 )
 from isofit.core.common import spectral_response_function
 
 Logger = logging.getLogger(__file__)
+
+
+@ray.remote(num_cpus=1)
+def predict_M(
+    M_Z,
+    M_lambda,
+    M_theta,
+    M_h,
+    points,
+    G_Xproj_vectors,
+    G_Xproj_values,
+    G_Xmean,
+    G_Xstd,
+):
+    Z_tr = M_Z.T  # training inputs
+
+    Z_te = reduce_points(points, G_Xproj_vectors, G_Xproj_values, G_Xmean, G_Xstd)
+    Z_te = Z_te * M_lambda  # scale test inputs
+    theta = M_theta
+
+    # RBF component to cross covariance matrix. Start by computing
+    # Euclidean distances between testing and training inputs
+    wb = np.sqrt(
+        (-2 * (Z_tr @ Z_te.T).T + np.sum(Z_tr * Z_tr, axis=1)).T
+        + np.sum(Z_te * Z_te, axis=1)
+    )
+
+    # Figure out a way to read kernel type from M["kernel"]["k"]
+    # Matern32
+    wb = np.sqrt(3.0) / theta[1] * wb  # h in kernel_functions.jl
+    wb = theta[0] * (1.0 + wb) * np.exp(-wb)
+    # Matern52
+    # wb = sqrt(5.) / theta[1] * wb # h in kernel_functions.jl
+    # wb = theta[0] * (1. + wb + wb**2 / sqrt(3.)) * exp(-wb)
+
+    # linear component to cross covariance matrix
+    wb2 = theta[2] * Z_tr @ Z_te.T
+    return (wb + wb2).T @ M_h
+
+
+def reduce_points(points, Xproj_vectors, Xproj_values, Xmu, Xsigma):
+    Z = (points - Xmu) / Xsigma
+    H = Xproj_vectors.T / Xproj_values
+    return Z @ H
 
 
 class KernelFlowsRT(object):
@@ -105,45 +150,47 @@ class KernelFlowsRT(object):
         nzycols = len(MVM.keys()) - 1  # take out GPGeometry from list of GPs
         ZY_pred = np.zeros((nte, nzycols))
 
+        logging.info(f"num parallel nzycols: {len(range(nzycols))}")
+
+        ZY_pred_calls = []
         for i in range(nzycols):
             M = MVM["M" + str(i + 1)]
             G = MVM["G"]  # shorthand
-            Z = self.reduce_points(
-                points, G["Xproj" + str(i + 1)], G["Xmean"], G["Xstd"]
+
+            # Z = reduce_points(
+            #    points, G["Xproj" + str(i + 1)], G["Xmean"], G["Xstd"]
+            # )
+            # ZY_pred[:, i] = predict_M(M, points, G["Xproj" + str(i + 1)], G["Xmean"], G["Xstd"])
+            M_Z = np.array(M["Z"])
+            M_lambda = np.array(M["lambda"])
+            M_theta = np.array(M["theta"])
+            M_h = np.array(M["h"])
+            G_Xproj_vectors = np.array(G["Xproj" + str(i + 1)]["vectors"])
+            G_Xproj_values = np.array(G["Xproj" + str(i + 1)]["values"])
+            G_Xmean = np.array(G["Xmean"])
+            G_Xstd = np.array(G["Xstd"])
+            points_ray = ray.put(points)
+
+            ZY_pred_calls.append(
+                predict_M.remote(
+                    M_Z,
+                    M_lambda,
+                    M_theta,
+                    M_h,
+                    points_ray,
+                    G_Xproj_vectors,
+                    G_Xproj_values,
+                    G_Xmean,
+                    G_Xstd,
+                )
             )
-            ZY_pred[:, i] = self.predict_M(M, Z)
+        ZY_out = ray.get(ZY_pred_calls)
+        for i in range(len(ZY_out)):
+            ZY_pred[:, i] = ZY_out[i]
+        del ZY_out
 
         # H is same as in recover() in dimension_reduction.jl
         MP = self.srf_matrix @ G["Yproj"]["vectors"][:, :].T
         H = MP * G["Yproj"]["values"][:]
         srfmean = self.srf_matrix @ G["Ymean"]
         return (ZY_pred @ H.T) * G["Ystd"] + srfmean
-
-    def predict_M(self, M, Z_te):
-        Z_tr = M["Z"][:, :].T  # training inputs
-        Z_te = Z_te * M["lambda"][:]  # scale test inputs
-        theta = M["theta"][:]
-
-        # RBF component to cross covariance matrix. Start by computing
-        # Euclidean distances between testing and training inputs
-        wb = np.sqrt(
-            (-2 * (Z_tr @ Z_te.T).T + np.sum(Z_tr * Z_tr, axis=1)).T
-            + np.sum(Z_te * Z_te, axis=1)
-        )
-
-        # Figure out a way to read kernel type from M["kernel"]["k"]
-        # Matern32
-        wb = np.sqrt(3.0) / theta[1] * wb  # h in kernel_functions.jl
-        wb = theta[0] * (1.0 + wb) * np.exp(-wb)
-        # Matern52
-        # wb = sqrt(5.) / theta[1] * wb # h in kernel_functions.jl
-        # wb = theta[0] * (1. + wb + wb**2 / sqrt(3.)) * exp(-wb)
-
-        # linear component to cross covariance matrix
-        wb2 = theta[2] * Z_tr @ Z_te.T
-        return (wb + wb2).T @ M["h"][:]
-
-    def reduce_points(self, points, Xproj, Xmu, Xsigma):
-        Z = (points - Xmu) / Xsigma
-        H = Xproj["vectors"][:, :].T / Xproj["values"][:]
-        return Z @ H
