cf33da1f121bcb2fee955cdd5664e2875d1a4681
unbohn <nbohn.1988@googlemail.com>
2024-05-09 14:34:08 -0700

    Updated download-and-unpack-sRTMnet.sh.

--- scripts/download-and-unpack-sRTMnet.sh
+++ scripts/download-and-unpack-sRTMnet.sh
@@ -13,7 +13,7 @@ set -o nounset
 SRTMNET_DIR="sRTMnet_v100"
 SRTMNET_PATH="${SRTMNET_DIR}/sRTMnet_v100.h5"
 SRTMNET_MODEL_FILENAME="sRTMnet_v100.h5"
-SRTMNET_ZIPFILENAME="sRTMnet_v100.zip"
+SRTMNET_AUX_FILENAME="sRTMnet_v100_aux.npz"
 
 # If the desired sRTM net file already exists just assume it is correct and do
 # nothing.
@@ -34,23 +34,4 @@ wget \
 wget \
    --no-verbose \
    --directory-prefix "${SRTMNET_DIR}" \
-   "https://zenodo.org/record/4096627/files/${SRTMNET_ZIPFILENAME}"
-
- # Unpack and delete some extraneous files.
- #
- # Run as a subprocess to allow 'cd'-ing into a subdirectory while automatically
- # returning to the current working directory once the subprocess exits.
- (
-
-   cd "${SRTMNET_DIR}"
-   unzip -o "${SRTMNET_DIR}"
-
-   # Remove MacOS specific files that the original author erroneously included
-   # in the archive.
-   rm -rf "__MACOSX"
-   rm -f .DS_Store sRTMnet_v100/.DS_Store
-
-   # The archive is quite large and no longer needed.
-   rm "${SRTMNET_ZIPFILENAME}"
-
- )
+   "https://avng.jpl.nasa.gov/pub/PBrodrick/isofit/${SRTMNET_AUX_FILENAME}"
