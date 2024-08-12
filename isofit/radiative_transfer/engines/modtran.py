60997472b3306f6f789b694a110a6db7988cd858
David R Thompson <davidraythompson@gmail.com>
2024-08-12 13:05:35 -0700

    Placate black

--- isofit/radiative_transfer/engines/modtran.py
+++ isofit/radiative_transfer/engines/modtran.py
@@ -315,8 +315,8 @@ class ModtranRT(RadiativeTransferEngine):
 
         # Translate to the MODTRAN OBSZEN convention
         if vals["OBSZEN"]<90:
-            vals["OBSZEN"] = 180-abs(vals["OBSZEN"])
-            
+            vals["OBSZEN"] = 180 - abs(vals["OBSZEN"])
+
         # Check rebuild conditions: LUT is missing or from a different config
         infilename = "LUT_" + filename_base + ".json"
         infilepath = os.path.join(self.sim_path, "LUT_" + filename_base + ".json")
