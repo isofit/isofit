0aa6f0b8714aaf18a918d571bac020a48875b3da
James Montgomery <github@booms.dev>
2024-09-09 13:46:43 -0700

    Removed redundancy

--- isofit/radiative_transfer/engines/modtran.py
+++ isofit/radiative_transfer/engines/modtran.py
@@ -318,10 +318,6 @@ class ModtranRT(RadiativeTransferEngine):
 
         modtran_config_str, modtran_config = self.modtran_driver(dict(vals))
 
-        # Translate to the MODTRAN OBSZEN convention
-        if vals["OBSZEN"] < 90:
-            vals["OBSZEN"] = 180 - abs(vals["OBSZEN"])
-
         # Check rebuild conditions: LUT is missing or from a different config
         infilename = "LUT_" + filename_base + ".json"
         infilepath = os.path.join(self.sim_path, "LUT_" + filename_base + ".json")
