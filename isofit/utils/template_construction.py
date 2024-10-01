d41cd76c04c311cc8de465a86ae8564915a0a941
unbohn <nbohn.1988@googlemail.com>
2024-09-30 17:08:00 -0700

    Added check for observer zenith MODTRAN convention in prebuilt LUT.

--- isofit/utils/template_construction.py
+++ isofit/utils/template_construction.py
@@ -854,6 +854,13 @@ def build_main_config(
     if prebuilt_lut_path is not None:
         ncds = nc.Dataset(prebuilt_lut_path, "r")
 
+        # first, check if observer zenith angle in prebuilt LUT comes in MODTRAN convention
+        # and convert lut grid as needed
+        if any(np.array(ncds["observer_zenith"]) > 90.0):
+            to_sensor_zenith_lut_grid = np.sort(
+                [180 - x for x in to_sensor_zenith_lut_grid]
+            )
+
         radiative_transfer_config["radiative_transfer_engines"]["vswir"]["lut_names"][
             "H2OSTR"
         ] = get_lut_subset(h2o_lut_grid)
