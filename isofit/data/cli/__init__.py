43a625a8ca39bfc8728f1100200e31866eead34d
James Montgomery <github@booms.dev>
2024-10-15 11:10:22 -0700

    Added findModules function to discover new download additions; fixed cube examples not building due to missing directory

--- isofit/data/cli/__init__.py
+++ isofit/data/cli/__init__.py
@@ -1,8 +1,22 @@
-import click
+import pkgutil
 
 from isofit.data import env
-from isofit.data.cli import data, examples, image_cube, modtran, sixs, srtmnet
 from isofit.data.download import cli
+
+
+def findModules():
+    """
+    Finds all of the available download modules
+
+    Returns
+    -------
+    dict
+        Dict of valid modules
+    """
+    return {
+        name: imp.find_module(name).load_module(name)
+        for imp, name, _ in pkgutil.iter_modules(__path__)
+    }
 
 
 @cli.download.command(name="all")
@@ -10,15 +24,15 @@ def download_all():
     """\
     Downloads all ISOFIT extra dependencies to the locations specified in the isofit.ini file using latest tags and versions.
     """
-    modules = [data, examples, sixs, srtmnet]
+    mods = findModules().values()
     pad = "=" * 16
 
-    for i, module in enumerate(modules):
-        click.echo(f"{pad} Beginning download {i+1} of {len(modules)} {pad}")
+    for i, module in enumerate(mods):
+        print(f"{pad} Beginning download {i+1} of {len(mods)} {pad}")
         module.download()
-        click.echo()
+        print()
 
-    click.echo("Finished all downloads")
+    print("Finished all downloads")
 
 
 @cli.validate.command(name="all")
@@ -26,24 +40,25 @@ def validate_all():
     """\
     Validates all ISOFIT extra dependencies at the locations specified in the isofit.ini file.
     """
-    modules = [data, examples, sixs, srtmnet]
+    mods = findModules().values()
     pad = "=" * 16
 
-    for i, module in enumerate(modules):
-        click.echo(f"{pad} Validating {i+1} of {len(modules)} {pad}")
+    for i, module in enumerate(mods):
+        print(f"{pad} Validating {i+1} of {len(mods)} {pad}")
         module.validate()
-        click.echo()
+        print()
 
-    click.echo("Finished all validations")
+    print("Finished all validations")
 
 
 def env_validate(keys, **kwargs):
     """
     Utility function for the `env` object to quickly validate specific dependencies
     """
+    mods = findModules()
     all_valid = True
     for key in keys:
-        module = globals().get(key)
+        module = mods.get(key)
         if module is None:
             print(f"Product not found: {key}")
             all_valid = False
