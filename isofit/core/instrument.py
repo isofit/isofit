72878c2ae6158388a90fc5384bbb797dc9da5433
Evan Greenberg <ev.ben.green@gmail.com>
2024-12-17 11:45:39 -0800

    Instrument sampling to handle single length lut elements with different RT and Instrument wl

--- isofit/core/instrument.py
+++ isofit/core/instrument.py
@@ -267,8 +267,16 @@ class Instrument:
         ):
             return rdn_hi
         wl, fwhm = self.calibration(x_instrument)
-        if rdn_hi.ndim == 1:
-            return resample_spectrum(rdn_hi, wl_hi, wl, fwhm)
+
+        # If rdn_hi is a vector of length 1, return itself
+        if rdn_hi.ndim == 1 and len(rdn_hi) <= 1:
+            return rdn_hi
+
+        # If rdn_hi is a vector of length > 1, return it resampled to instrument
+        elif rdn_hi.ndim == 1 and len(rdn_hi) > 1:
+            return resample_spectrum(np.copy(rdn_hi), wl_hi, wl, fwhm)
+
+        # If rdn_hi is a multidim array, do the multidim resampling
         else:
             resamp = []
             # The "fast resample" option approximates a complete resampling
@@ -280,7 +288,7 @@ class Instrument:
                     resamp.append(interp1d(wl_hi, blur)(wl))
             else:
                 for i, r in enumerate(rdn_hi):
-                    r2 = resample_spectrum(r, wl_hi, wl, fwhm)
+                    r2 = resample_spectrum(np.copy(r), wl_hi, wl, fwhm)
                     resamp.append(r2)
             return np.array(resamp)
 
