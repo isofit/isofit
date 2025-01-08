5a0a796712a39602d8b3c9b9516e095e06ccc627
James Montgomery <J.Montgomery@jpl.nasa.gov>
2025-01-08 09:36:05 -0800

    Fixed version->file

--- isofit/test/test_cli.py
+++ isofit/test/test_cli.py
@@ -90,7 +90,7 @@ def test_apply_oe(files, args, surface):
     ray.shutdown()
     sleep(120)
 
-    args[3] = env.path("srtmnet", key="srtmnet.version")
+    args[3] = env.path("srtmnet", key="srtmnet.file")
     arguments = ["apply_oe", *files, *args, "--surface_path", surface]
 
     # Passing non-string arguments to click is not allowed.
