Fixed derivative updating RT quantities
Updated drfl_dglint

Updated drfl_dsurface and perturbation

Annotations

Reverting derivative changes

Added note on fixed apparent rfl in dRT, and testing slow example

isofit/core/forward.py
@@ -184,7 +184,11 @@ class ForwardModel:
 
         return block_diag(Sa_surface, Sa_RT, Sa_instrument)
 
-    def calc_surface_quantities(self, x_surface, geom, L_down_dir, L_down_dif):
+    def upsample_surface_to_RT(self, x_surface, geom, L_down_dir, L_down_dif):
+        """Upsample the surface quantities to the RT wavelength grid.
+        The forward calculation is done at RT wavelengths.
+        Then downsampled to instrument.
+        """
         # Call surface reflectance and derivative w.r.t. surface, upsample
         rho_dir_dir, rho_dif_dir = self.calc_rfl(
             x_surface, geom, L_down_dir, L_down_dif
@@ -210,34 +214,6 @@ class ForwardModel:
             drfl_dsurface_hi,
             Ls_hi,
             dLs_dsurface_hi,
-        )
-
-    def calc_RT_quantities(self, x_RT, geom):
-        # Propogate LUT
-        r = self.RT.get_shared_rtm_quantities(x_RT, geom)
-
-        # Default: get directional radiances
-        L_dir_dir, L_dif_dir, L_dir_dif, L_dif_dif = self.RT.get_L_coupled(r, geom)
-        L_tot = L_dir_dir + L_dif_dir + L_dir_dif + L_dif_dif
-
-        # Handle case for 1c vs 4c model
-        if not isinstance(L_tot, np.ndarray) or len(L_tot) == 1:
-            # 1c model w/in if clause
-            L_tot, L_down_dir, L_down_dif = self.RT.get_L_down_transmitted(x_RT, geom)
-        else:
-            # 4c model w/in else clause
-            L_down_dir = L_dir_dir + L_dif_dir
-            L_down_dif = L_dif_dir + L_dif_dir
-
-        return (
-            r,
-            L_tot,
-            L_down_dir,
-            L_down_dif,
-            L_dir_dir,
-            L_dif_dir,
-            L_dir_dif,
-            L_dif_dif,
         )
 
     def calc_rdn(
@@ -293,11 +269,11 @@ class ForwardModel:
             L_dif_dir,
             L_dir_dif,
             L_dif_dif,
-        ) = self.calc_RT_quantities(x_RT, geom)
+        ) = self.RT.calc_RT_quantities(x_RT, geom)
 
         # Get Surface quantities - handles upsampling
-        (rho_dir_dir, rho_dif_dir, _, Ls, _) = self.calc_surface_quantities(
-            x_surface, geom, L_down_dir, L_down_dif
+        (rho_dir_dir, rho_dif_dir, drfl_dsurface, Ls, dLs_dsurface) = (
+            self.upsample_surface_to_RT(x_surface, geom, L_down_dir, L_down_dif)
         )
 
         rdn = self.calc_rdn(
@@ -367,11 +343,20 @@ class ForwardModel:
             L_dif_dir,
             L_dir_dif,
             L_dif_dif,
-        ) = self.calc_RT_quantities(x_RT, geom)
+        ) = self.RT.calc_RT_quantities(x_RT, geom)
 
-        # Get Surface quantities - handles upsampling
+        # Get Surface quantities and sample them at RT wavelengths
+        """NOTE: Due to current limitations in the scope of RT vs.
+        surface, we have to fix the apparent surface reflectance
+        before calculating the numerical derivative w.r.t the atmosphere.
+        This is an issue for surface terms that are coupled with the atmopshere.
+        For example the shapes of the glint effects (g_dir and g_dif) are dependent
+        on L_dir / L_tot and L_dif / L_tot respectively, and should vary w/in dRT.
+        The current implementation assumes a constant apparent reflectance,
+        and therefore constant surface-atmosphere coupled terms (e.g. g_dir and g_dif).
+        """
         (rho_dir_dir, rho_dif_dir, drfl_dsurface, Ls, dLs_dsurface) = (
-            self.calc_surface_quantities(x_surface, geom, L_down_dir, L_down_dif)
+            self.upsample_surface_to_RT(x_surface, geom, L_down_dir, L_down_dif)
         )
 
         # Need to pass calc rdn into instrument derivative
@@ -392,16 +377,10 @@ class ForwardModel:
         # To get the derivative w.r.t. RT
         drdn_dRT = self.RT.drdn_dRT(
             x_RT,
+            geom,
             rho_dir_dir,
             rho_dif_dir,
             Ls,
-            L_tot,
-            L_dir_dir,
-            L_dif_dir,
-            L_dir_dif,
-            L_dif_dif,
-            r,
-            geom,
             rdn,
         )
 
@@ -416,7 +395,7 @@ class ForwardModel:
             L_dir_dir + L_dir_dif,
         )
 
-        # To get derivatives w.r.t. measurement
+        # To get derivatives w.r.t. instrument, downsample to instrument wavelengths
         dmeas_dsurface = self.instrument.sample(
             x_instrument, self.RT.wl, drdn_dsurface.T
         ).T
@@ -452,11 +431,11 @@ class ForwardModel:
             L_dif_dir,
             L_dir_dif,
             L_dif_dif,
-        ) = self.calc_RT_quantities(x_RT, geom)
+        ) = self.RT.calc_RT_quantities(x_RT, geom)
 
-        # Get Surface quantities - handles upsampling
-        (rho_dir_dir, rho_dif_dir, _, Ls, _) = self.calc_surface_quantities(
-            x_surface, geom, L_down_dir, L_down_dif
+        # Get Surface quantities and sample them at RT wavelengths
+        (rho_dir_dir, rho_dif_dir, drfl_dsurface, Ls, dLs_dsurface) = (
+            self.upsample_surface_to_RT(x_surface, geom, L_down_dir, L_down_dif)
         )
 
         rdn = self.calc_rdn(
@@ -475,18 +454,14 @@ class ForwardModel:
 
         drdn_dRTb = self.RT.drdn_dRTb(
             x_RT,
+            geom,
             rho_dir_dir,
             rho_dif_dir,
             Ls,
-            L_tot,
-            L_dir_dir,
-            L_dif_dir,
-            L_dir_dif,
-            L_dif_dif,
-            r,
-            geom,
             rdn,
         )
+
+        # To get derivatives w.r.t. instrument, downsample to instrument wavelengths
         dmeas_dRTb = self.instrument.sample(x_instrument, self.RT.wl, drdn_dRTb.T).T
         dmeas_dinstrumentb = self.instrument.dmeas_dinstrumentb(
             x_instrument, self.RT.wl, rdn

isofit/radiative_transfer/radiative_transfer.py
@@ -183,8 +183,6 @@ class RadiativeTransfer:
         Physics-based forward model to calculate at-sensor radiance.
         Includes topography, background reflectance, and glint.
         """
-        coszen, cos_i = geom.check_coszen_and_cos_i(self.coszen)
-
         # Adjacency effects
         # ToDo: we need to think about if we want to obtain the background reflectance from the Geometry object
         #  or from the surface model, i.e., the same way as we do with the target pixel reflectance
@@ -406,26 +404,68 @@ class RadiativeTransfer:
 
         return L_dir_dir, L_dif_dir, L_dir_dif, L_dif_dif
 
-    def drdn_dRT(
-        self,
-        x_RT,
-        rho_dir_dir,
-        rho_dif_dir,
-        Ls,
-        L_tot,
-        L_dir_dir,
-        L_dif_dir,
-        L_dir_dif,
-        L_dif_dif,
-        r,
-        geom,
-        rdn,
-    ):
+    def calc_RT_quantities(self, x_RT: np.ndarray, geom: Geometry):
+        """Retrieves the RT quantities including the LUT sample (r),
+        and the radiances (L). This function handles the hand-off between
+        the 1c and 4c model.
 
+        In the 1c case, L_dir_dir, L_dif_dir, L_dir_dif, L_dif_dif = 0,
+        and L_tot, L_down_dir, and L_down_dif are populated within the
+        if statement.
+
+        In the 4c case, we always use returns from get_L_coupled
+        """
+        # Propogate LUT
+        r = self.get_shared_rtm_quantities(x_RT, geom)
+
+        # Default: get directional radiances
+        L_dir_dir, L_dif_dir, L_dir_dif, L_dif_dif = self.get_L_coupled(r, geom)
+        L_tot = L_dir_dir + L_dif_dir + L_dir_dif + L_dif_dif
+
+        # Handle case for 1c vs 4c model
+        if not isinstance(L_tot, np.ndarray) or len(L_tot) == 1:
+            # 1c model w/in if clause
+            L_tot, L_down_dir, L_down_dif = self.get_L_down_transmitted(x_RT, geom)
+        else:
+            # 4c model w/in else clause
+            L_down_dir = L_dir_dir + L_dif_dir
+            L_down_dif = L_dif_dir + L_dif_dir
+
+        return (
+            r,
+            L_tot,
+            L_down_dir,
+            L_down_dif,
+            L_dir_dir,
+            L_dif_dir,
+            L_dir_dif,
+            L_dif_dif,
+        )
+
+    def drdn_dRT(self, x_RT, geom, rho_dir_dir, rho_dif_dir, Ls, rdn):
+        """Derivative of estimated radiance w.r.t. RT statevector elements.
+        We use a numerical approach to approximate dRT with a constant surface
+        reflectance. This is a reasonable approx. for the multicomponent surface.
+
+        When using the glint model however, this does not take into account
+        the dependence of the surface reflectance on the atmosphere.
+        """
         # perturb each element of the RT state vector (finite difference)
         K_RT = []
         x_RTs_perturb = x_RT + np.eye(len(x_RT)) * eps
         for x_RT_perturb in list(x_RTs_perturb):
+            (
+                r,
+                L_tot,
+                L_down_dir,
+                L_down_dif,
+                L_dir_dir,
+                L_dif_dir,
+                L_dir_dif,
+                L_dif_dif,
+            ) = self.calc_RT_quantities(x_RT_perturb, geom)
+
+            # Surface state is held constant?
             rdne = self.calc_rdn(
                 x_RT_perturb,
                 rho_dir_dir,
@@ -440,33 +480,26 @@ class RadiativeTransfer:
                 geom,
             )
             K_RT.append((rdne - rdn) / eps)
+
         K_RT = np.array(K_RT).T
 
         return K_RT
 
-    def drdn_dRTb(
-        self,
-        x_RT,
-        rho_dir_dir,
-        rho_dif_dir,
-        Ls,
-        L_tot,
-        L_dir_dir,
-        L_dif_dir,
-        L_dir_dif,
-        L_dif_dif,
-        r,
-        geom,
-        rdn,
-    ):
+    def drdn_dRTb(self, x_RT, geom, rho_dir_dir, rho_dif_dir, Ls, rdn):
+        """Derivative of estimated rdn w.r.t. H2O_ABSCO
 
+        Currently, the K_b matrix only covers forward model derivatives
+        due to H2O_ABSCO unknowns, so that subsequent errors might occur
+        when water vapor is not part of the statevector
+        (which is very unlikely though).
+        """
         if len(self.bvec) == 0:
             Kb_RT = np.zeros((0, len(self.wl.shape)))
-        # currently, the K_b matrix only covers forward model derivatives due to H2O_ABSCO unknowns,
-        # so that subsequent errors might occur when water vapor is not part of the state
-        # vector (which is very unlikely though). the following statement captures this case,
-        # but might need to be modified as soon as we add more unknowns
+
         # ToDo: might require modification in case more unknowns are added
+        # The following statement captures the case that H2O is not part
+        # of the statevector.
+        # but might need to be modified as soon as we add more unknowns
         elif len(self.bvec) > 0 and "H2OSTR" not in self.statevec_names:
             Kb_RT = np.zeros((1, len(self.wl)))
         else:
@@ -480,6 +513,17 @@ class RadiativeTransfer:
                     i = self.statevec_names.index("H2OSTR")
                     x_RT_perturb = x_RT.copy()
                     x_RT_perturb[i] = x_RT[i] * perturb
+                    (
+                        r,
+                        L_tot,
+                        L_down_dir,
+                        L_down_dif,
+                        L_dir_dir,
+                        L_dif_dir,
+                        L_dir_dif,
+                        L_dif_dif,
+                    ) = self.calc_RT_quantities(x_RT_perturb, geom)
+
                     rdne = self.calc_rdn(
                         x_RT_perturb,
                         rho_dir_dir,

isofit/surface/surface_glint_model.py
@@ -40,7 +40,7 @@ class GlintModelSurface(MultiComponentSurface):
         )  # Numbers from Marcel Koenig; used for prior mean
 
         # Special glint bounds
-        rmin, rmax = -0.02, 2.0
+        rmin, rmax = -0.05, 2.0
         self.bounds = [[rmin, rmax] for w in self.wl]
         self.bounds.extend([[-1, 10], [0, 10]])  # Gege (2021), WASI user manual
         self.n_state = self.n_state + 2
@@ -137,15 +137,20 @@ class GlintModelSurface(MultiComponentSurface):
 
     def drfl_dsurface(self, x_surface, geom, L_down_dir=None, L_down_dif=None):
         """Partial derivative of reflectance with respect to state vector,
-        calculated at x_surface."""
+        calculated at x_surface.
+
+        We have found that this arrangement provides the most stable results.
+        However, we need to double check the math. This implementation
+        reflects a need to apply the chain rule to construct the full derivative
+        where the dependence of Rho_dif on alpha_dif (and g_dif) has to be
+        incorporated. Discuss."""
         drfl = self.dlamb_dsurface(x_surface, geom)
 
         g_dir, g_dif = self.glint_spectra(geom, L_down_dir, L_down_dif)
+        # drfl = drfl * np.reshape(g_dif, (len(g_dif), 1))
 
         # TODO make the indexing better for the surface state elements
-        # Sun glint derivative
         drfl[:, self.glint_ind] = g_dir
-        # Sky glint derivative
         drfl[:, self.glint_ind + 1] = g_dif
 
         return drfl
@@ -180,6 +185,7 @@ class GlintModelSurface(MultiComponentSurface):
 
         # Glint derivatives
         drdn_dgdd, drdn_dgdsf = self.drdn_dglint(L_tot, L_down_dir, s_alb, rho_dif_dir)
+
         # Store the glint derivatives as last two rows in drdn_drfl
         drdn_drfl[:, -2] = drdn_dgdd
         drdn_drfl[:, -1] = drdn_dgdsf
@@ -240,6 +246,7 @@ class GlintModelSurface(MultiComponentSurface):
         # ep = (L_dif_dir + L_dif_dif) + ((L_tot * background * g_dif) / (1 - background))
         # ep = np.reshape(ep, (len(ep), 1))
         # H = np.append(H, ep, axis=1)
+        # TODO test adding this back in
 
         return H
 

isofit/utils/analytical_line.py
@@ -30,13 +30,15 @@ from spectral.io import envi
 
 from isofit import ray
 from isofit.configs import configs
-from isofit.core.common import envi_header, load_spectrum
+from isofit.core.common import envi_header, eps, load_spectrum
 from isofit.core.fileio import IO, write_bil_chunk
 from isofit.core.forward import ForwardModel
 from isofit.core.geometry import Geometry
 from isofit.inversion.inverse import Inversion
-from isofit.inversion.inverse_simple import invert_analytical
+from isofit.inversion.inverse_simple import invert_algebraic, invert_analytical
 from isofit.utils.atm_interpolation import atm_interpolation
+
+PERTURB = 0.03
 
 
 def analytical_line(
@@ -348,19 +350,21 @@ class Worker(object):
                     x_RT = rt_state[r, c, :]
 
                 iv_idx = self.fm.surface.analytical_iv_idx
-                superpixel_state = subs_state[int(lbl[r, c, 0]), 0, iv_idx]
+                init_state = subs_state[int(lbl[r, c, 0]), 0, iv_idx]
+                # init_state += PERTURB
+                # init_state += np.abs(np.random.normal(scale=eps))
 
                 # Concatenate full statevector to use for initialization
                 # This only works with the correct indexing.
                 # Any surface component of x_RT has to be first in the array.
                 # TODO handle indexing in a safer way. See line 126
-                x0 = np.concatenate([superpixel_state, x_RT])
+                x0 = np.concatenate([init_state, x_RT])
                 states, unc = invert_analytical(
                     self.iv.fm,
                     self.iv.winidx,
                     meas,
                     geom,
-                    x0,
+                    np.copy(x0),
                     self.num_iter,
                     self.hash_table,
                     self.hash_size,
@@ -372,15 +376,15 @@ class Worker(object):
 
             # What do we want to do with the negative reflectances?
             state = output_state[r - start_line, ...]
-            mask = np.logical_and.reduce(
-                [
-                    state < self.rfl_bounds[0],
-                    state > self.rfl_bounds[1],
-                    state != -9999,
-                    state != -0.01,
-                ]
-            )
-            state[mask] = 0
+            # mask = np.logical_and.reduce(
+            #     [
+            #         state < self.rfl_bounds[0],
+            #         state > self.rfl_bounds[1],
+            #         state != -9999,
+            #         state != -0.01,
+            #     ]
+            # )
+            # state[mask] = 0
 
             logging.info(f"Analytical line writing line {r}")
 