671c72284e78c23edd84945494b714e746befbd0
Phil Brodrick <phil.brodrick@gmail.com>
2025-02-21 06:39:42 -0800

    Update __init__.py
    version update

--- isofit/__init__.py
+++ isofit/__init__.py
@@ -18,7 +18,7 @@
 # Author: David R Thompson, david.r.thompson@jpl.nasa.gov
 #         Philip G Brodrick, philip.brodrick@jpl.nasa.gov
 #
-__version__ = "3.3.5"
+__version__ = "3.3.7"
 
 import logging
 
