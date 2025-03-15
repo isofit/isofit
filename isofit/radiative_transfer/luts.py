e92b7a5fb9f41c7d4c9b3854b0c2d7969c0c327d
Phil Brodrick <phil.brodrick@gmail.com>
2025-03-15 16:26:31 -0400

    change to zlib compression

--- isofit/radiative_transfer/luts.py
+++ isofit/radiative_transfer/luts.py
@@ -116,7 +116,7 @@ class Create:
                 dimensions=dims,
                 fill_value=fill_value,
                 chunksizes=chunksizes,
-                compression="zstd",
+                compression="zlib",
             )
             var[:] = vals
 
