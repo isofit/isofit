074588ab2c2fc379020b33d2c8da2f16d3aa901b
Evan Greenberg <ev.ben.green@gmail.com>
2025-06-09 16:39:37 -0700

    Added small unit conversion functions

--- isofit/core/units.py
+++ isofit/core/units.py
@@ -17,7 +17,7 @@
 # ISOFIT: Imaging Spectrometer Optimal FITting
 # Authors: Philip G. Brodrick, philip.brodrick@jpl.nasa.gov
 #          Niklas Bohn, urs.n.bohn@jpl.nasa.gov
-#          Evan Greenberg
+#          Evan Greenberg, evan.greenberg@jpl.nasa.gov
 
 import numpy as np
 
@@ -78,3 +78,208 @@ def rdn_to_transm(rdn, coszen, solar_irr):
     """
     transm = rdn / E_to_L(solar_irr, coszen)
     return transm
+
+
+def m_wavenumber_to_nm(wavenumber):
+    """
+    Function to convert wavenumber to nm.
+
+    Args:
+        wavenumber:     value(s) in wavenumber in 1/m
+
+    Returns:
+        value(s) in nanometers
+    """
+    nm = 1e9 / (wavenumber)
+    return nm
+
+
+def cm_wavenumber_to_nm(wavenumber):
+    """
+    Function to convert wavenumber to nm.
+
+    Args:
+        wavenumber:     value(s) in wavenumber in 1/cm
+
+    Returns:
+        value(s) in nanometers
+    """
+    nm = 1e7 / (wavenumber)
+    return nm
+
+
+def micron_to_nm(micron):
+    """
+    Function to convert microns to nanometers
+
+    Args:
+        micron: value(s) in microns
+
+    Returns:
+        value(s) in nanometers
+    """
+    nm = micron * 1000
+    return nm
+
+
+def nm_to_micron(nm):
+    """
+    Function to convert nanometers to microns
+
+    Args:
+        nm:     value(s) in nanometers
+
+    Returns:
+        value(s) in microns
+    """
+    micron = nm / 1000
+    return micron
+
+
+def vis_to_aod(vis):
+    """
+    Converts VIS modtran parameter to the AOD550 .
+    This formula comes from page 50 of the MODTRAN 6 user manual,
+    which relates ViS to the extinction coefficient at 550 nm.
+    The constant, 0.01159 is the Rayleigh scattering coefficient at
+    550 nm in 1/km.
+
+    Args:
+        vis:    visibility
+
+    Returns:
+        Data vector converted to exctinction at 550 nm
+    """
+    aod = (np.log(50) / vis) - 0.01159
+    return aod
+
+
+def aod_to_vis(aod):
+    """
+    Converts AOD550 to VIS modtran parameter.
+    Formula comes from page 50 of the MODTRAN 6 users manual,
+    which relates ViS to the extinction coefficient at 550 nm.
+    The constant, 0.01159 is the Rayleigh scattering coefficient at
+    550 nm in 1/km.
+
+    Args:
+        aod:    extinction at 550 nm
+
+    Returns:
+        Data vector converted to visibility
+    """
+    vis = np.log(50) / (aod + 0.01159)
+    return vis
+
+
+def rfl_to_rrs(rfl):
+    """
+    Converts unitless reflectance to remote sensing reflectance
+
+    Args:
+        rfl:    unitless reflectance
+
+    Returns:
+        Data vector of remote sensing reflectance in units 1/sr
+    """
+    rrs = rfl / np.pi
+    return rrs
+
+
+def rrs_to_rfl(rrs):
+    """
+    Converts remote sensing reflectance to unitless reflectance
+
+    Args:
+        rrs:   remote sensing reflectance in 1/sr
+
+    Returns:
+        Data vector of unitless reflectance
+    """
+    rfl = rrs * np.pi
+    return rfl
+
+
+def Wm2_to_uWcm2(wm):
+    """
+    Converts value of units Watts / square meter to units
+    of micro-watts / square centimeter
+
+    Args:
+        wm:     value(s) in units of W/m2
+
+    Returns:
+        Value(s) in units of uW/cm2
+    """
+    uwcm = wm * 1e2
+    return uwcm
+
+
+def uWcm2_to_Wm2(uwcm):
+    """
+    Converts value of micro-watts / square centimeter to units Watts / square meter
+
+    Args:
+        uwcm:     value(s) in units of uW/cm2
+
+    Returns:
+        Value(s) in units of W/m2
+    """
+    wm = uwcm / 1e2
+    return wm
+
+
+def m_to_km(m):
+    """
+    Converts value of units meters to units kilometer
+
+    Args:
+        m:     value(s) in units of meters
+
+    Returns:
+        Value(s) in units of kilometers
+    """
+    km = m / 1000
+    return km
+
+
+def km_to_m(km):
+    """
+    Converts value of units kilometers to unit meters
+
+    Args:
+        km:     value(s) in units of kilometers
+
+    Returns:
+        Value(s) in units of meters
+    """
+    m = km * 1000
+    return m
+
+
+def m_to_ft(m):
+    """
+    Converts value of units meters to units feet
+
+    Args:
+        m:     value(s) in units of meters
+
+    Returns:
+        Value(s) in units of feet
+    """
+    ft = m * 3.280839895
+    return ft
+
+
+def ft_to_m(ft):
+    """
+    Converts value of units ft to units m
+
+    Args:
+        ft:     value(s) in units of feet
+
+    Returns:
+        Value(s) in units of meters
+    """
+    m = ft / 3.280839895
+    return m
