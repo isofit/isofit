3bb3ea878826ebebe954f568bd1a76f2410dd9b6
James Montgomery <J.Montgomery@jpl.nasa.gov>
2025-10-08 12:40:35 -0700

    Added debug statement

--- isofit/data/cli/sixs.py
+++ isofit/data/cli/sixs.py
@@ -81,6 +81,8 @@ def make(directory, stdout=subprocess.PIPE, stderr=subprocess.PIPE, debug=False)
     if platform.system() == "Windows":
         make = "mingw32-make.exe"
 
+    print(os.environ["PATH"])
+
     # Now make it
     try:
         call = subprocess.run(
