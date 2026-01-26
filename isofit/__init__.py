4a8a0b7bdc9eb56ae3b1433b7636503acb847866
unbohn <108841813+unbohn@users.noreply.github.com>
2026-01-26 14:28:23 -0800

    Bump version number

--- isofit/__init__.py
+++ isofit/__init__.py
@@ -18,7 +18,7 @@
 # Author: David R Thompson, david.r.thompson@jpl.nasa.gov
 #         Philip G Brodrick, philip.brodrick@jpl.nasa.gov
 #
-__version__ = "3.7.0"
+__version__ = "3.7.1"
 
 
 import logging
