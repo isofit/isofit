03ba882eaa7243db684158d58d2e46c0ce8e8b06
brentwilder <brentonwilder1995@gmail.com>
2026-02-24 10:16:12 -0600

    Improving terrain view math by including local slope

--- isofit/radiative_transfer/radiative_transfer.py
+++ isofit/radiative_transfer/radiative_transfer.py
@@ -372,11 +372,15 @@ class RadiativeTransfer:
         # Re-reflection from nearby surface contributing to at surface signal
         # Assumptions: no atmospheric attenuation, and reflectance is isotropic over the field of view
         if geom.bg_rfl is not None:
+
+            # Assumed flat surface and background sky view of 1 for converting to terrain view
             skyview_factor_avg = 1.0
-            t = 1 + (
-                (geom.bg_rfl * (1 - skyview_factor))
-                / (1 - geom.bg_rfl * (1 - skyview_factor_avg))
-            )
+            cos_slope = 1.0
+
+            v_t = (1 + cos_slope) / 2 - skyview_factor
+            v_t_avg = (1 + cos_slope) / 2 - skyview_factor_avg
+            t = 1 + ((geom.bg_rfl * v_t) / (1 - geom.bg_rfl * v_t_avg))
+
             L_dir_dir *= t
             L_dif_dir *= t
             L_dir_dif *= t
