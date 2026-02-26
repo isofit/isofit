329350e62426a460d8c0ffcb776c0e5904216ba1
James Montgomery <github@booms.dev>
2026-02-26 11:05:21 -0800

    Added a thread lock on file write to safeguard possible conflicts

--- isofit/debug/resource_tracker.py
+++ isofit/debug/resource_tracker.py
@@ -411,6 +411,8 @@ class FileResources(ResourceTracker):
 
         super().__init__(callback=self.write, **kwargs)
 
+        self._lock = threading.Lock()
+
         self.file = Path(file)
         self.file.parent.mkdir(exist_ok=True, parents=True)
 
@@ -435,19 +437,20 @@ class FileResources(ResourceTracker):
         """
         data = json.dumps(info) + "\n"
 
-        try:
-            self.io.write(data)
-            self.io.flush()
-        except ValueError:
-            # Raised when the file is closed, likely caused by the main process hitting an exception
-            # In those cases, just quietly stop the thread
-            self.stop()
-        except:
-            # Other reasons should report
-            logging.exception(
-                "Encountered unknown exception when attempting to write resources"
-            )
-            self.stop()
+        with self._lock:
+            try:
+                self.io.write(data)
+                self.io.flush()
+            except ValueError:
+                # Raised when the file is closed, likely caused by the main process hitting an exception
+                # In those cases, just quietly stop the thread
+                self.stop()
+            except:
+                # Other reasons should report
+                logging.exception(
+                    "Encountered unknown exception when attempting to write resources"
+                )
+                self.stop()
 
 
 def stream(file: str, sleep: float = 0.2) -> dict:
