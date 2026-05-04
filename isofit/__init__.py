c6cc0f85543fbd68aa7823eafcda27ae94a6cc63
unbohn <108841813+unbohn@users.noreply.github.com>
2026-05-04 14:15:49 -0700

    Bump version from 3.7.4 to 3.7.5

--- isofit/__init__.py
+++ isofit/__init__.py
@@ -18,7 +18,7 @@
 # Author: David R Thompson, david.r.thompson@jpl.nasa.gov
 #         Philip G Brodrick, philip.brodrick@jpl.nasa.gov
 #
-__version__ = "3.7.5"
+__version__ = "3.7.6"
 
 
 import logging
