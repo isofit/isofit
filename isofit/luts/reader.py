4ec67123eb7978e04961808bc520c18597bd0194
James Montgomery <github@booms.dev>
2026-07-13 17:21:28 -0700

    Re-added sel() for now

--- isofit/luts/reader.py
+++ isofit/luts/reader.py
@@ -102,6 +102,72 @@ def _inspect_zarr_dimensions(lut_path: Path) -> dict[str, np.ndarray]:
     return lut_dimensions
 
 
+def sel(ds, dim, lt=None, lte=None, gt=None, gte=None, encompass=True):
+    """
+    Subselects an xarray Dataset object using .sel
+
+    Parameters
+    ----------
+    ds: xarray.Dataset
+        LUT dataset
+    dim: str
+        Dimension to work on
+    lt: float, default=None
+        Select along this dim coordinates that are valued less than this
+    lte: float, default=None
+        Select along this dim coordinates that are valued less than or equal to this
+    gt: float, default=None
+        Select along this dim coordinates that are valued greater than this
+    gte: float, default=None
+        Select along this dim coordinates that are valued greater than or equal to this
+    encompass: bool, default=True
+        Change the values of gte/lte such that these values are encompassed using the
+        previous/next valid grid point
+
+    Returns
+    -------
+    ds: xarray.Dataset
+        Subsetted dataset
+    """
+    assert None in (
+        lt,
+        lte,
+    ), f"Subsetting `lt` and `lte` are mutually exclusive, please only set one for dim {dim}"
+    assert None in (
+        gt,
+        gte,
+    ), f"Subsetting `gt` and `gte` are mutually exclusive, please only set one for dim {dim}"
+
+    # Which index in a where to select -- if the dim is in reverse order, this needs to swap
+    g, l = -1, 0
+    if ds[dim][0] > ds[dim][-1]:
+        g, l = 0, -1
+
+    if lt is not None:
+        ds = ds.sel({dim: ds[dim] < lt})
+
+    elif lte is not None:
+        if encompass:
+            where = ds[dim].where(ds[dim] > lte).dropna(dim)
+            lte = where[l] if where.size else ds[dim].max()
+            Logger.debug(f"Encompass changed lte value to {lte}")
+
+        ds = ds.sel({dim: ds[dim] <= lte})
+
+    if gt is not None:
+        ds = ds.sel({dim: gt < ds[dim]})
+
+    elif gte is not None:
+        if encompass:
+            where = ds[dim].where(ds[dim] < gte).dropna(dim)
+            gte = where[g] if where.size else ds[dim].min()
+            Logger.debug(f"Encompass changed gte value to {gte}")
+
+        ds = ds.sel({dim: gte <= ds[dim]})
+
+    return ds
+
+
 def sub(ds: xr.Dataset, dim: str, strat) -> xr.Dataset:
     """
     Subsets a dataset object along a specific dimension in a few supported ways.
