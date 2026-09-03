4b30f821f7f0725eeda45b5b2c5281e76eab47c2
brentwilder <brentonwilder1995@gmail.com>
2026-09-03 15:00:32 -0500

    Enforce max_slope for bgtopo as well

--- isofit/core/geometry.py
+++ isofit/core/geometry.py
@@ -104,20 +104,6 @@ class Geometry:
                 + self.path_length_km * np.cos(np.deg2rad(self.observer_zenith))
             )
 
-        # Set flat assumptions for the background topography if no information is given
-        if self.skyview_factor_bg is None:
-            self.skyview_factor_bg = 1.0
-
-        if self.slope_bg is None:
-            self.cos_slope_bg = 1.0
-        else:
-            self.cos_slope_bg = np.cos(np.radians(self.slope_bg))
-
-        if self.slope is None:
-            self.cos_slope = 1.0
-        else:
-            self.cos_slope = np.cos(np.radians(self.slope))
-
         # Determine how to treat coszen
         self.use_universal_coszen = True
 
@@ -188,6 +174,22 @@ class Geometry:
             # Set flat assumptions for the background topography if no information is given
             if self.cos_i_bg is None:
                 self.cos_i_bg = self.coszen
+
+            if self.skyview_factor_bg is None:
+                self.skyview_factor_bg = 1.0
+
+            # Enforce max slope for both target pixel slope and background slope
+            if self.slope_bg is None:
+                self.cos_slope_bg = 1.0
+            else:
+                self.cos_slope_bg = np.cos(
+                    np.radians(min(self.slope_bg, self.max_slope))
+                )
+
+            if self.slope is None:
+                self.cos_slope = 1.0
+            else:
+                self.cos_slope = np.cos(np.radians(min(self.slope, self.max_slope)))
 
         else:
             logging.warning(
